356abb2edf7ac2edfe48fae59b2fdc74ff3c6b8b
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-11-04 23:08:41 -0800

    add driver and lex/parse/code-gen arg parsing

--- main.c
+++ main.c
@@ -47,21 +47,37 @@ int
 main(int argc, char *argv[])
 {
 	enum {
+		ACTION_LEX_PARSE_ASM_EMIT,
+		ACTION_LEX,
+		ACTION_LEX_PARSE,
+		ACTION_LEX_PARSE_ASM,
 		ACTION_USAGE_HELP,
 		ACTION_USAGE_ERROR,
 	} action = 0;
 
 	int synonym = 0;
 	struct option lo[] = {
+		{"code-gen", no_argument, &synonym, 'c'},
 		{"help", no_argument, &synonym, 'h'},
+		{"lex", no_argument, &synonym, 'l'},
+		{"parse", no_argument, &synonym, 'p'},
 		{NULL, 0, NULL, 0},
 	};
 
 	int opt = 0;
 	while ((opt = getopt_long(argc, argv, "h", lo, NULL)) != -1) {
 		switch (opt == 0 ? synonym : opt) {
+		case 'c':
+			action = MAX(action, ACTION_LEX_PARSE_ASM);
+			break;
 		case 'h':
 			action = MAX(action, ACTION_USAGE_HELP);
+			break;
+		case 'l':
+			action = MAX(action, ACTION_LEX);
+			break;
+		case 'p':
+			action = MAX(action, ACTION_LEX_PARSE);
 			break;
 		default:
 			action = MAX(action, ACTION_USAGE_ERROR);
@@ -73,6 +89,18 @@ main(int argc, char *argv[])
 	FILE *out = stderr; /* assume output to stderr by default */
 
 	switch (action) {
+	case ACTION_LEX_PARSE_ASM_EMIT:
+		rc = result_to_status(RESULT_OK);
+		break;
+	case ACTION_LEX:
+		rc = result_to_status(RESULT_OK);
+		break;
+	case ACTION_LEX_PARSE:
+		rc = result_to_status(RESULT_OK);
+		break;
+	case ACTION_LEX_PARSE_ASM:
+		rc = result_to_status(RESULT_OK);
+		break;
 	case ACTION_USAGE_HELP:
 		rc = EX_OK;
 		out = stdout;
---
+++ scripts/driver.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+set -euxo pipefail
+
+DRIVER_MODE=""
+INPUT_FILE=""
+
+if [ $# == 1 ] ; then
+	INPUT_FILE="$1"
+elif [ $# == 2 ] ; then
+	DRIVER_MODE="$1"
+	INPUT_FILE="$2"
+fi
+
+OUTPUT_FILE="${INPUT_FILE%%.*}"
+PREPROCESSED_FILE="$OUTPUT_FILE.i"
+ASSEMBLY_FILE="$OUTPUT_FILE.s"
+
+CC=$(which gcc)
+NQCC=$(realpath "$0/../../build/enqcc")
+
+$CC -E -P "$INPUT_FILE" -o "$PREPROCESSED_FILE"
+$NQCC "$DRIVER_MODE" "$PREPROCESSED_FILE" "$ASSEMBLY_FILE"
+$CC "$ASSEMBLY_FILE" -o "$OUTPUT_FILE"
+
+rm -f "$PREPROCESSED_FILE" "$ASSEMBLY_FILE"
