5425b9626e5cbada122340c4ad6117a2ced066ca
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-11-04 23:08:41 -0800

    add driver and lex/parse/code-gen arg parsing

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include "passes.h"
 #include "result.h"
 
 #include <errno.h>
@@ -10,25 +11,64 @@
 #include <sysexits.h>
 #include <unistd.h> /* for close() */
 
+static __attribute__((format(printf, 1, 2))) void
+to_stderr(const char *pattern, ...)
+{
+	va_list ap = {0};
+	va_start(ap, pattern);
+	fprintf(stderr, "ERROR: ");
+	vfprintf(stderr, pattern, ap);
+	fputc('\n', stderr);
+	va_end(ap);
+}
+
+static __attribute__((warn_unused_result)) int
+result_to_status(result_t r)
+{
+	auto_result owner = r;
+	if (owner.err) {
+		auto_result_str str = result_to_str(owner);
+		to_stderr("%s", str ? str : "[result_to_str() -> NULL]");
+		return EX_SOFTWARE;
+	}
+	return EX_OK;
+}
+
 int
 main(int argc, char *argv[])
 {
 	enum {
+		ACTION_LEX_PARSE_ASM_EMIT,
+		ACTION_LEX,
+		ACTION_LEX_PARSE,
+		ACTION_LEX_PARSE_ASM,
 		ACTION_USAGE_HELP,
 		ACTION_USAGE_ERROR,
 	} action = 0;
 
 	int synonym = 0;
 	struct option lo[] = {
+		{"code-gen", no_argument, &synonym, 'c'},
 		{"help", no_argument, &synonym, 'h'},
+		{"lex", no_argument, &synonym, 'l'},
+		{"parse", no_argument, &synonym, 'p'},
 		{NULL, 0, NULL, 0},
 	};
 
 	int opt = 0;
 	while ((opt = getopt_long(argc, argv, "h", lo, NULL)) != -1) {
 		switch (opt == 0 ? synonym : opt) {
+		case 'c':
+			action = MAX(action, ACTION_LEX_PARSE_ASM);
+			break;
 		case 'h':
 			action = MAX(action, ACTION_USAGE_HELP);
+			break;
+		case 'l':
+			action = MAX(action, ACTION_LEX);
+			break;
+		case 'p':
+			action = MAX(action, ACTION_LEX_PARSE);
 			break;
 		default:
 			action = MAX(action, ACTION_USAGE_ERROR);
@@ -40,6 +80,25 @@ main(int argc, char *argv[])
 	FILE *out = stderr; /* assume output to stderr by default */
 
 	switch (action) {
+	case ACTION_LEX_PARSE_ASM_EMIT:
+		rc = result_to_status(RESULT_OK);
+		break;
+	case ACTION_LEX:
+		if (optind >= argc) {
+			to_stderr("Missing input file argument");
+		} else {
+			struct token *tok = NULL;
+			rc = result_to_status(lex_init(argv[optind], &tok));
+			debug_lex_output(tok);
+			lex_free(tok);
+		}
+		break;
+	case ACTION_LEX_PARSE:
+		rc = result_to_status(RESULT_OK);
+		break;
+	case ACTION_LEX_PARSE_ASM:
+		rc = result_to_status(RESULT_OK);
+		break;
 	case ACTION_USAGE_HELP:
 		rc = EX_OK;
 		out = stdout;
---
+++ src/lex.c
@@ -0,0 +1,179 @@
+#include "passes.h"
+#include "sys/compiler_features.h"
+#include "sys/debug.h"
+#include "sys/tmpfile.h"
+
+#include <assert.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h> /* for memset() */
+
+static WARN_UNUSED result_t
+lex_alloc(struct token **tok)
+{
+	*tok = malloc(sizeof(**tok));
+	check_if(*tok == NULL, ERR_LEX_ALLOC);
+	memset(*tok, 0, sizeof(**tok));
+	return RESULT_OK;
+}
+
+static WARN_UNUSED result_t
+lex_alloc_stringview(struct string_view **sv, const char *data, size_t sz)
+{
+	*sv = malloc(sizeof(**sv));
+	check_if(*sv == NULL, ERR_LEX_ALLOC);
+	(*sv)->data = data;
+	(*sv)->sz = sz;
+	return RESULT_OK;
+}
+
+static WARN_UNUSED bool
+lex_peek_ok(const char c)
+{
+	if (isspace(c)) {
+		return true;
+	} else if (c == '(' || c == ')' || c == '{' || c == '}' || c == ';') {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+result_t
+lex_init(const char *src, struct token **tok)
+{
+	int fd = open(src, O_RDONLY);
+	check_if(fd < 0, ERR_LEX_OPEN_SOURCE_FILE, errno, src);
+
+	struct string_view code; // TODO: munmap in lex_free()
+	check(tmpmap(fd, &code));
+
+	const char *pos = code.data;
+	while (pos < code.data + code.sz) {
+		if (isspace(*pos)) {
+			++pos;
+			continue;
+		}
+
+		check(lex_alloc(tok));
+		struct token *cur = *tok;
+
+		if (*pos == '(') {
+			cur->token_type = TOKEN_PAREN_OPEN;
+		} else if (*pos == ')') {
+			cur->token_type = TOKEN_PAREN_CLOSE;
+		} else if (*pos == '{') {
+			cur->token_type = TOKEN_BRACE_OPEN;
+		} else if (*pos == '}') {
+			cur->token_type = TOKEN_BRACE_CLOSE;
+		} else if (*pos == ';') {
+			cur->token_type = TOKEN_SEMICOLON;
+		} else if (isdigit(*pos)) {
+			cur->token_type = TOKEN_CONSTANT;
+			const char *start = pos;
+			do {
+				++pos;
+			} while (isdigit(*pos));
+			const size_t sz = pos - start;
+			check(lex_alloc_stringview(&cur->value, start, sz));
+			check_if(!lex_peek_ok(*pos),
+			         ERR_LEX_IDENTIFIER_CONSTANT_KEYWORD_PEEK_ERROR,
+			         *pos,
+			         start,
+			         sz);
+			--pos; // allow generic increment to handle last char
+		} else if (isalpha(*pos) || *pos == '_') {
+			const char *start = pos;
+			do {
+				++pos;
+			} while (isalnum(*pos) || *pos == '_');
+			const size_t sz = pos - start;
+			if (0 == strncmp("return", start, sz)) {
+				cur->token_type = TOKEN_KEYWORD_RETURN;
+			} else if (0 == strncmp("void", start, sz)) {
+				cur->token_type = TOKEN_KEYWORD_VOID;
+			} else if (0 == strncmp("int", start, sz)) {
+				cur->token_type = TOKEN_KEYWORD_INT;
+			} else {
+				check(lex_alloc_stringview(&cur->value,
+				                           start,
+				                           sz));
+				cur->token_type = TOKEN_IDENTIFIER;
+			}
+			check_if(!lex_peek_ok(*pos),
+			         ERR_LEX_IDENTIFIER_CONSTANT_KEYWORD_PEEK_ERROR,
+			         *pos,
+			         start,
+			         sz);
+			--pos; // allow generic increment to handle last char
+		} else {
+			const size_t remaining = code.sz - (pos - code.data);
+			return make_result(ERR_LEX_NO_MATCH, pos, remaining);
+		}
+
+		tok = &cur->next;
+		++pos;
+	}
+
+	return RESULT_OK;
+}
+
+void
+lex_free(struct token *tok)
+{
+	while (tok != NULL) {
+		struct token *tmp = tok;
+		tok = tok->next;
+		free(tmp);
+	}
+}
+
+static void
+debug_lex_one(struct token *tok)
+{
+	switch (tok->token_type) {
+	case TOKEN_IDENTIFIER:
+		assert(tok->value != NULL);
+		debug("IDENTIFIER %.*s", (int)tok->value->sz, tok->value->data);
+		break;
+	case TOKEN_CONSTANT:
+		assert(tok->value != NULL);
+		debug("CONSTANT %.*s", (int)tok->value->sz, tok->value->data);
+		break;
+	case TOKEN_KEYWORD_RETURN:
+		debug("KEYWORD return");
+		break;
+	case TOKEN_KEYWORD_VOID:
+		debug("KEYWORD void");
+		break;
+	case TOKEN_KEYWORD_INT:
+		debug("KEYWORD int");
+		break;
+	case TOKEN_PAREN_OPEN:
+		debug("PAREN open");
+		break;
+	case TOKEN_PAREN_CLOSE:
+		debug("PAREN close");
+		break;
+	case TOKEN_BRACE_OPEN:
+		debug("BRACE open");
+		break;
+	case TOKEN_BRACE_CLOSE:
+		debug("BRACE close");
+		break;
+	case TOKEN_SEMICOLON:
+		debug("SEMICOLON");
+		break;
+	}
+}
+
+void
+debug_lex_output(struct token *tok)
+{
+	while (tok != NULL) {
+		debug_lex_one(tok);
+		tok = tok->next;
+	}
+}
