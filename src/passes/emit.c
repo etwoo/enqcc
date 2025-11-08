7c1d88df03d25e6ab43a309386187c1c8e45b70b
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-11-07 19:50:10 -0800

    pretty-print zero offset dereference to just (...) instead of 0(...)

--- src/passes/emit.c
+++ src/passes/emit.c
@@ -56,10 +56,14 @@ emit_asm_operand(const struct asm_operand *o, int fd)
 		assert(0 && "PSEUDOREGISTER should have been eliminated");
 		break;
 	case ASM_OPERAND_STACK:
-		dprintf(fd,
-		        "%lld(%s)",
-		        -1 * CODEGEN_BYTES_PER_VALUE * o->u.num,
-		        STR_REG_RBP);
+		if (o->u.num == 0) {
+			dprintf(fd, "(%s)", STR_REG_RBP);
+		} else {
+			dprintf(fd,
+				"%lld(%s)",
+				-1 * CODEGEN_BYTES_PER_VALUE * o->u.num,
+				STR_REG_RBP);
+		}
 		break;
 	}
 }
