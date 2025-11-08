857ae5e9a85574a82d6645d26ad3ac9bef644b6d
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-11-07 19:36:13 -0800

    checkpoint emit.c

--- src/passes/emit.c
+++ src/passes/emit.c
@@ -8,15 +8,18 @@
 
 static const char LINUX_NX[] = "\t.section .note.GNU-stack,\"\",@progbits\n";
 static const char MACOS_FUNC_PREFIX[] = "_";
+static const char STR_OP_MOV_QUAD[] = "movq";
 static const char STR_OP_MOV[] = "movl";
 static const char STR_OP_NEG[] = "negl";
 static const char STR_OP_NOT[] = "notl";
+static const char STR_OP_POP_QUAD[] = "popq";
+static const char STR_OP_PUSH_QUAD[] = "pushq";
 static const char STR_OP_RET[] = "ret";
-static const char STR_OP_SUB[] = "subq";
-static const char STR_REGISTER_EAX[] = "%eax";
-static const char STR_REGISTER_R10[] = "%r10d";
-static const char STR_REGISTER_RSP[] = "%rsp"; /* aka frame pointer */
-static const char STR_REGISTER_RBP[] = "%rbp"; /* aka stack pointer */
+static const char STR_OP_SUB_QUAD[] = "subq";
+static const char STR_REG_EAX[] = "%eax";
+static const char STR_REG_R10[] = "%r10d";
+static const char STR_REG_RSP[] = "%rsp"; /* aka frame pointer */
+static const char STR_REG_RBP[] = "%rbp"; /* aka stack pointer */
 
 static void
 emit_asm_footer(enum platform plat, int fd)
@@ -39,13 +42,13 @@ emit_asm_operand(const struct asm_operand *o, int fd)
 	case ASM_OPERAND_REGISTER:
 		switch (o->u.reg) {
 		case ASM_REGISTER_AX:
-			dprintf(fd, "%s", STR_REGISTER_EAX);
+			dprintf(fd, "%s", STR_REG_EAX);
 			break;
 		case ASM_REGISTER_R10:
-			dprintf(fd, "%s", STR_REGISTER_R10);
+			dprintf(fd, "%s", STR_REG_R10);
 			break;
 		case ASM_REGISTER_RSP:
-			dprintf(fd, "%s", STR_REGISTER_RSP);
+			dprintf(fd, "%s", STR_REG_RSP);
 			break;
 		}
 		break;
@@ -56,7 +59,7 @@ emit_asm_operand(const struct asm_operand *o, int fd)
 		dprintf(fd,
 		        "%lld(%s)",
 		        -1 * CODEGEN_BYTES_PER_VALUE * o->u.num,
-		        STR_REGISTER_RBP);
+		        STR_REG_RBP);
 		break;
 	}
 }
@@ -73,7 +76,7 @@ emit_asm_op(const struct asm_op *op, int fd)
 		dprintf(fd, "%s", STR_OP_MOV);
 		break;
 	case ASM_OP_SUB:
-		dprintf(fd, "%s", STR_OP_SUB);
+		dprintf(fd, "%s", STR_OP_SUB_QUAD);
 		break;
 	case ASM_OP_UNARY_NEG:
 		dprintf(fd, "%s", STR_OP_NEG);
@@ -114,6 +117,8 @@ emit_asm(const struct assembly *cg, enum platform plat, int fd)
 
 	const struct string_view *fname = &cg->function.identifier;
 	dprintf(fd, "%s%.*s:\n", fprefix, (int)fname->sz, fname->data);
+	dprintf(fd, "%s %s\n", STR_OP_PUSH_QUAD, STR_REG_RBP);
+	dprintf(fd, "%s %s %s\n", STR_OP_MOV_QUAD, STR_REG_RSP, STR_REG_RBP);
 
 	for (struct asm_op *op = cg->function.ops; op != NULL; op = op->next) {
 		emit_asm_op(op, fd);
