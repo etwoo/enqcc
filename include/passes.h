fc363ccfc4e6c19953bb89f006c50febc140e3d0
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-11-08 00:06:10 -0800

    fix clang-format error in passes.h

--- include/passes.h
+++ include/passes.h
@@ -34,7 +34,7 @@ result_t codegen_init(const struct intermediate *ir, struct assembly **cg)
 result_t codegen_replace_pseudoregisters(struct assembly *cg)
 	__attribute__((warn_unused_result));
 result_t codegen_fixup_instructions(const struct intermediate *ir,
-                                   struct assembly *cg)
+                                    struct assembly *cg)
 	__attribute__((warn_unused_result));
 void codegen_free(struct assembly *cg);
 void codegen_cleanup(struct assembly **cg);
