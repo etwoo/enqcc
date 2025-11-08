fcdf3e1f7fbbcfdb6f76facf72ef7c36d0213b09
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-11-08 04:24:51 -0800

    checkpoint: parsing for + and -

--- src/passes/parse.c
+++ src/passes/parse.c
@@ -10,21 +10,12 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-static WARN_UNUSED result_t
-parse_alloc_helper(struct ast **a, unsigned node_type)
-{
-	assert(a != NULL && *a == NULL);
-	*a = malloc(sizeof(**a));
-	check_if(*a == NULL, ERR_LEX_ALLOC);
-	memset(*a, 0, sizeof(**a));
-	(**a).node_type = node_type;
-	return RESULT_OK;
-}
-
-#define parse_alloc(a, nt)                                                     \
+#define parse_alloc(expr, nt)                                                  \
 	do {                                                                   \
-		check(parse_alloc_helper((a), (nt)));                          \
-		assert(*(a)); /* silence clang-analyzer NullDereference */     \
+		(expr) = malloc(sizeof(*(expr)));                              \
+		check_if((expr) == NULL, ERR_LEX_ALLOC);                       \
+		memset(expr, 0, sizeof(*(expr)));                              \
+		(*(expr)).node_type = nt;                                      \
 	} while (0);
 
 static WARN_UNUSED bool
@@ -43,7 +34,7 @@ static WARN_UNUSED result_t
 parse_constant(const struct token **tok, struct ast **dst)
 {
 	assert(is_token_type(*tok, TOKEN_CONSTANT));
-	parse_alloc(dst, NODE_CONSTANT_INT);
+	parse_alloc(*dst, NODE_CONSTANT_INT);
 
 	/*
 	 * strtoll() does not update errno on success, so we must clear it
@@ -69,33 +60,33 @@ parse_identifier(const struct token **tok, struct ast **dst)
 	if (!is_token_type(*tok, TOKEN_IDENTIFIER)) {
 		return make_result(ERR_PARSE_FUNC_NAME_EXPECT_TOKEN_IDENTIFIER);
 	}
-	parse_alloc(dst, NODE_IDENTIFIER);
+	parse_alloc(*dst, NODE_IDENTIFIER);
 
 	(**dst).u.str = (**tok).val;
 	token_consume(tok);
 	return RESULT_OK;
 }
 
+static result_t parse_expression(const struct token **tok,
+                                 struct ast **dst) WARN_UNUSED;
+
 static WARN_UNUSED result_t
-parse_expression(const struct token **tok, struct ast **dst)
+parse_factor(const struct token **tok, struct ast **dst)
 {
 	assert(!is_token_type(*tok, TOKEN_HYPHEN_HYPHEN)); // unimplemented
-
-	// TODO: parse binary ops like TOKEN_PLUS_SIGN
-
 	if (is_token_type(*tok, TOKEN_CONSTANT)) {
-		parse_alloc(dst, NODE_EXPRESSION_UNARY_IDENTITY);
+		parse_alloc(*dst, NODE_EXPRESSION_UNARY_IDENTITY);
 		check(parse_constant(tok, &(**dst).u.op_unary.operand));
 	} else if (is_token_type(*tok, TOKEN_TILDE)) {
-		parse_alloc(dst, NODE_EXPRESSION_UNARY_COMPLEMENT);
+		parse_alloc(*dst, NODE_EXPRESSION_UNARY_COMPLEMENT);
 		token_consume(tok);
-		check(parse_expression(tok, &(**dst).u.op_unary.operand));
+		check(parse_factor(tok, &(**dst).u.op_unary.operand));
 	} else if (is_token_type(*tok, TOKEN_HYPHEN)) {
-		parse_alloc(dst, NODE_EXPRESSION_UNARY_NEGATE);
+		parse_alloc(*dst, NODE_EXPRESSION_UNARY_NEGATE);
 		token_consume(tok);
-		check(parse_expression(tok, &(**dst).u.op_unary.operand));
+		check(parse_factor(tok, &(**dst).u.op_unary.operand));
 	} else if (is_token_type(*tok, TOKEN_PAREN_OPEN)) {
-		parse_alloc(dst, NODE_EXPRESSION_PAREN_ENCLOSED);
+		parse_alloc(*dst, NODE_EXPRESSION_PAREN_ENCLOSED);
 		token_consume(tok);
 		check(parse_expression(tok, &(**dst).u.op_unary.operand));
 		if (!is_token_type(*tok, TOKEN_PAREN_CLOSE)) {
@@ -110,9 +101,44 @@ parse_expression(const struct token **tok, struct ast **dst)
 }
 
 static WARN_UNUSED result_t
+parse_expression(const struct token **tok, struct ast **dst)
+{
+	struct ast *left __attribute__((cleanup(parse_cleanup))) = NULL;
+	check(parse_factor(tok, &left));
+
+	while (true) {
+		struct ast *bop __attribute__((cleanup(parse_cleanup))) = NULL;
+		if (is_token_type(*tok, TOKEN_PLUS_SIGN)) {
+			parse_alloc(bop, NODE_EXPRESSION_BINARY_ADD);
+		} else if (is_token_type(*tok, TOKEN_HYPHEN)) {
+			parse_alloc(bop, NODE_EXPRESSION_BINARY_SUBTRACT);
+		} else {
+			break;
+		}
+		token_consume(tok);
+
+		struct ast *right __attribute__((cleanup(parse_cleanup))) =
+			NULL;
+		// NOLINT(clang-analyzer-unix.Malloc)
+		check(parse_factor(tok, &right));
+
+		bop->u.op_binary.lhs = left;
+		bop->u.op_binary.rhs = right;
+		left = bop;
+
+		right = NULL; /* release ownership */
+		bop = NULL;   /* release ownership */
+	}
+
+	*dst = left;
+	left = NULL; /* release ownership */
+	return RESULT_OK;
+}
+
+static WARN_UNUSED result_t
 parse_statement(const struct token **tok, struct ast **dst)
 {
-	parse_alloc(dst, NODE_EXPRESSION_UNARY_IDENTITY);
+	parse_alloc(*dst, NODE_EXPRESSION_UNARY_IDENTITY);
 
 	if (!is_token_type(*tok, TOKEN_KEYWORD_RETURN)) {
 		return make_result(ERR_PARSE_STMT_EXPECT_TOKEN_KEYWORD_RETURN);
@@ -132,7 +158,7 @@ parse_statement(const struct token **tok, struct ast **dst)
 static WARN_UNUSED result_t
 parse_function(const struct token **tok, struct ast **dst)
 {
-	parse_alloc(dst, NODE_FUNCTION);
+	parse_alloc(*dst, NODE_FUNCTION);
 
 	if (!is_token_type(*tok, TOKEN_KEYWORD_INT)) {
 		return make_result(ERR_PARSE_FUNC_EXPECT_RETURN_TYPE_INT);
@@ -174,7 +200,7 @@ parse_function(const struct token **tok, struct ast **dst)
 result_t
 parse_init(const struct token *tok, struct ast **a)
 {
-	parse_alloc(a, NODE_PROGRAM);
+	parse_alloc(*a, NODE_PROGRAM);
 
 	check(parse_function(&tok, &(**a).u.program.entrypoint_function));
 	if (tok != NULL) {
