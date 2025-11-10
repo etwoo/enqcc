7d306e8dee0d434e4e43c8ab1e7eb95e35281b32
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-11-09 21:27:36 -0800

    ch 1: minimal 4-pass compiler for trivial C subset

---
+++ include/passes.h
@@ -0,0 +1,37 @@
+#ifndef COMPILER_PASSES_H
+#define COMPILER_PASSES_H
+
+#include "result.h"
+
+struct token;
+
+result_t lex_init(const char *src, struct token **tok)
+	__attribute__((warn_unused_result));
+void lex_free(struct token *tok);
+void lex_cleanup(struct token **tok);
+void lex_debug_print(const struct token *tok);
+
+struct ast;
+
+result_t parse_init(const struct token *tok, struct ast **a)
+	__attribute__((warn_unused_result));
+void parse_free(struct ast *a);
+void parse_cleanup(struct ast **a);
+void parse_debug_print(const struct ast *a, size_t indent);
+
+struct assembly;
+
+result_t codegen_init(const struct ast *a, struct assembly **cg)
+	__attribute__((warn_unused_result));
+void codegen_free(struct assembly *cg);
+void codegen_cleanup(struct assembly **cg);
+void codegen_debug_print(const struct assembly *cg);
+
+enum platform {
+	PLATFORM_MACOS,
+	PLATFORM_LINUX,
+};
+
+void emit_asm(const struct assembly *cg, enum platform plat, int fd);
+
+#endif
