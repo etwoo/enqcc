a716fc82b25ce985c0a1e9146b0e7945b812f34b
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-11-09 21:28:57 -0800

    ch 2: unary operators

--- src/passes/codegen.c
+++ src/passes/codegen.c
@@ -1,13 +1,16 @@
 #include "passes/codegen.h"
 
 #include "passes.h"
-#include "passes/parse.h"
+#include "passes/ir.h"
 #include "sys/array.h"
 #include "sys/compiler_features.h"
 #include "sys/debug.h"
 
 #include <assert.h>
+#include <stdbool.h>
 #include <stdlib.h>
+
+const long long int CODEGEN_BYTES_PER_VALUE = 4;
 
 #define codegen_alloc(dst)                                                     \
 	do {                                                                   \
@@ -16,76 +19,39 @@
 		memset(dst, 0, sizeof(*(dst)));                                \
 	} while (0)
 
-static WARN_UNUSED result_t
-codegen_statement(const struct ast_statement *a, struct asm_op **dst)
+static void
+codegen_op_list_concat(struct asm_op *first, struct asm_op *second)
 {
-	assert(a->base.node_type == NODE_STATEMENT);
-	assert(a->return_expression.base.node_type == NODE_EXPRESSION);
-	assert(a->return_expression.constant.base.node_type ==
-	       NODE_CONSTANT_INT);
-
-	assert(*dst == NULL);
-	codegen_alloc(*dst);
-	(*dst)->base.statement_type = ASM_OP_MOV;
-	(*dst)->args[0].operand_type = ASM_OPERAND_IMMEDIATE;
-	(*dst)->args[0].num = a->return_expression.constant.num;
-	(*dst)->args[1].operand_type = ASM_OPERAND_REGISTER_EAX;
-
-	assert((*dst)->next == NULL);
-	codegen_alloc((*dst)->next);
-	(*dst)->next->base.statement_type = ASM_OP_RET;
-
-	return RESULT_OK;
+	assert(first != NULL);
+	while (first->next != NULL) {
+		first = first->next;
+	}
+	assert(first->next == NULL);
+	first->next = second;
 }
 
-static WARN_UNUSED result_t
-codegen_function(const struct ast_function *a, struct asm_function *dst)
+static void
+codegen_op_list_prepend(struct asm_op *new_head, struct asm_op **head)
 {
-	assert(a->base.node_type == NODE_FUNCTION);
-	assert(dst->base.statement_type == ASM_FUNCTION);
-	dst->identifier = a->identifier.token;
-	check(codegen_statement(&a->statement, &dst->ops));
-	return RESULT_OK;
+	codegen_op_list_concat(new_head, *head);
+	*head = new_head;
 }
 
-static WARN_UNUSED result_t
-codegen_program(const struct ast_program *a, struct asm_program *dst)
+static void
+codegen_op_list_free(struct asm_op *cursor)
 {
-	assert(a->base.node_type == NODE_PROGRAM);
-	assert(dst->base.statement_type == ASM_PROGRAM);
-	dst->function.base.statement_type = ASM_FUNCTION;
-	check(codegen_function(&a->function, &dst->function));
-	return RESULT_OK;
-}
-
-result_t
-codegen_init(const struct ast *a, struct assembly **cg)
-{
-	struct asm_program *program = NULL;
-	codegen_alloc(program);
-	program->base.statement_type = ASM_PROGRAM;
-	*cg = &program->base;
-
-	check(codegen_program((const struct ast_program *)a, program));
-	return RESULT_OK;
+	while (cursor != NULL) {
+		struct asm_op *tmp = cursor;
+		cursor = cursor->next;
+		free(tmp);
+	}
 }
 
 void
 codegen_free(struct assembly *cg)
 {
-	if (cg != NULL) {
-		assert(cg->statement_type == ASM_PROGRAM);
-		struct asm_program *program = (struct asm_program *)cg;
-
-		struct asm_op *ops = program->function.ops;
-		while (ops != NULL) {
-			struct asm_op *tmp = ops;
-			ops = ops->next;
-			free(tmp);
-		}
-
-		free(cg);
-	}
+	codegen_op_list_free(cg ? cg->function.ops : NULL);
+	free(cg);
 }
 
 void
@@ -95,53 +61,299 @@ codegen_cleanup(struct assembly **cg)
 }
 
 static void
+codegen_map_operand(const struct ir_val *src, struct asm_operand *dst)
+{
+	switch (src->subtype) {
+	case IR_VAL_NONE:
+		assert(0 && "unset operand in 2-arg op");
+		break;
+	case IR_VAL_CONSTANT_INT:
+		dst->operand_type = ASM_OPERAND_IMMEDIATE;
+		dst->u.num = src->num;
+		break;
+	case IR_VAL_TEMPORARY_VARIABLE:
+		dst->operand_type = ASM_OPERAND_PSEUDO_REGISTER;
+		dst->u.num = src->num;
+		break;
+	}
+}
+
+static WARN_UNUSED result_t
+codegen_statement_one(const struct ir_op *src, struct asm_op **dst)
+{
+	assert(*dst == NULL);
+	codegen_alloc(*dst);
+
+	switch (src->opcode) {
+	case IR_OP_UNARY_IDENTITY:
+		(**dst).opcode = ASM_OP_MOV;
+		codegen_map_operand(&src->args[0], &(**dst).args[0]);
+		(**dst).args[1].operand_type = ASM_OPERAND_REGISTER;
+		(**dst).args[1].u.reg = ASM_REGISTER_AX;
+		dst = &(**dst).next;
+		codegen_alloc(*dst);
+		(**dst).opcode = ASM_OP_RET;
+		break;
+	case IR_OP_UNARY_NEGATE:
+	case IR_OP_UNARY_COMPLEMENT:
+		(**dst).opcode = ASM_OP_MOV;
+		for (size_t i = 0; i < ARRAY_SIZE((**dst).args); ++i) {
+			codegen_map_operand(&src->args[i], &(**dst).args[i]);
+		}
+		dst = &(**dst).next;
+		codegen_alloc(*dst);
+		switch (src->opcode) {
+		case IR_OP_UNARY_NEGATE:
+			(**dst).opcode = ASM_OP_UNARY_NEG;
+			break;
+		case IR_OP_UNARY_COMPLEMENT:
+			(**dst).opcode = ASM_OP_UNARY_NOT;
+			break;
+		default:
+			assert(0); /* logic error in caller */
+			break;
+		}
+		codegen_map_operand(&src->args[1], &(**dst).args[0]);
+		break;
+	}
+
+	return RESULT_OK;
+}
+
+static WARN_UNUSED result_t
+codegen_statement(const struct ir_op *src, struct asm_op **dst)
+{
+	while (src != NULL) {
+		check(codegen_statement_one(src, dst));
+		src = src->next;
+		while (*dst != NULL) {
+			dst = &(**dst).next;
+		}
+	}
+	return RESULT_OK;
+}
+
+static WARN_UNUSED result_t
+codegen_function(const struct ir_function *ir, struct asm_function *dst)
+{
+	dst->identifier = ir->identifier;
+	check(codegen_statement(ir->ops, &dst->ops));
+	return RESULT_OK;
+}
+
+result_t
+codegen_init(const struct intermediate *ir, struct assembly **cg)
+{
+	codegen_alloc(*cg);
+	check(codegen_function(&ir->function, &(**cg).function));
+	return RESULT_OK;
+}
+
+result_t
+codegen_replace_pseudoregisters(struct assembly *cg)
+{
+	debug("Replacing pseudoregisters with stack addresses");
+	for (struct asm_op *op = cg->function.ops; op != NULL; op = op->next) {
+		for (size_t i = 0; i < ARRAY_SIZE(op->args); ++i) {
+			struct asm_operand *arg = &op->args[i];
+			if (arg->operand_type == ASM_OPERAND_PSEUDO_REGISTER) {
+				arg->operand_type = ASM_OPERAND_STACK;
+			}
+		}
+	}
+	return RESULT_OK;
+}
+
+static WARN_UNUSED result_t
+codegen_fixup_alloc_stack(const struct intermediate *ir, struct assembly *cg)
+{
+	struct asm_op *alloc_stack = NULL;
+	codegen_alloc(alloc_stack);
+	alloc_stack->opcode = ASM_OP_SUB;
+	alloc_stack->args[0].operand_type = ASM_OPERAND_IMMEDIATE;
+	alloc_stack->args[0].u.num =
+		CODEGEN_BYTES_PER_VALUE * (ir->env.generator - 1);
+	alloc_stack->args[1].operand_type = ASM_OPERAND_REGISTER;
+	alloc_stack->args[1].u.reg = ASM_REGISTER_RSP;
+
+	codegen_op_list_prepend(alloc_stack, &cg->function.ops);
+	return RESULT_OK;
+}
+
+static void
+tr_cleanup(struct asm_op *pp[][2])
+{
+	codegen_op_list_free((*pp)[0]);
+	codegen_op_list_free((*pp)[1]);
+}
+
+static WARN_UNUSED result_t
+codegen_fixup_stack_to_stack(struct asm_op *prev,
+                             struct asm_op *cur,
+                             struct asm_op **new_prev,
+                             struct asm_op **new_cur)
+{
+	assert(prev && cur);
+	assert(prev->next == cur);
+
+	/*
+	 * Prepare a trampoline by memcpy()-ing invalid instructions like:
+	 *
+	 *     movl -4(%rbp), -8(%rbp)
+	 *
+	 * ... into temporary copies. Modify these temporaries to perform the
+	 * same logical operation in an actually-valid way:
+	 *
+	 *     movl -4(%rbp), %r10d
+	 *     movl %r10d, -8(%rbp)
+	 *
+	 * ... and then splice these new ops into the original containing list.
+	 */
+	struct asm_op *trampoline[2] __attribute__((cleanup(tr_cleanup))) = {0};
+
+	for (size_t i = 0; i < ARRAY_SIZE(trampoline); ++i) {
+		// NOLINTNEXTLINE(clang-analyzer-unix.Malloc)
+		codegen_alloc(trampoline[i]);
+		memcpy(trampoline[i], cur, sizeof(*cur));
+	}
+
+	trampoline[0]->next = NULL;
+	trampoline[0]->args[1].operand_type = ASM_OPERAND_REGISTER;
+	trampoline[0]->args[1].u.reg = ASM_REGISTER_R10;
+
+	trampoline[1]->next = NULL;
+	trampoline[1]->args[0].operand_type = ASM_OPERAND_REGISTER;
+	trampoline[1]->args[0].u.reg = ASM_REGISTER_R10;
+
+	/*
+	 * Split containing list at <cur>, excluding <cur> from both halves.
+	 */
+	struct asm_op *remainder = cur->next;
+	cur->next = NULL;
+	prev->next = NULL;
+
+	/*
+	 * Insert trampoline sublist into containing list.
+	 */
+	codegen_op_list_concat(prev, trampoline[0]);
+	codegen_op_list_concat(trampoline[0], trampoline[1]);
+	codegen_op_list_concat(trampoline[1], remainder);
+
+	/*
+	 * Prepare list cursor positions for next loop iteration.
+	 */
+	*new_prev = trampoline[1];
+	*new_cur = remainder;
+
+	/*
+	 * Release ownership of trampoline sublist to caller.
+	 */
+	for (size_t i = 0; i < ARRAY_SIZE(trampoline); ++i) {
+		trampoline[i] = NULL;
+	}
+
+	return RESULT_OK;
+}
+
+result_t
+codegen_fixup_instructions(const struct intermediate *ir, struct assembly *cg)
+{
+	debug("Fixing up invalid instructions");
+
+	if (ir->env.generator > 1) {
+		check(codegen_fixup_alloc_stack(ir, cg));
+	}
+
+	struct asm_op *prev = NULL;
+	struct asm_op *cur = cg->function.ops;
+	while (cur != NULL) {
+		if (cur->opcode != ASM_OP_MOV ||
+		    cur->args[0].operand_type != ASM_OPERAND_STACK ||
+		    cur->args[1].operand_type != ASM_OPERAND_STACK) {
+			prev = cur;
+			cur = cur->next;
+			continue;
+		}
+		assert(prev != NULL &&
+		       "should never need trampoline on very first op, which "
+		       "should always be something like setting up function "
+		       "context, substracting from frame pointer RSP, etc");
+		check(codegen_fixup_stack_to_stack(prev, cur, &prev, &cur));
+	}
+
+	return RESULT_OK;
+}
+
+static void
 codegen_debug_print_operand(const struct asm_operand *operand)
 {
 	switch (operand->operand_type) {
+	case ASM_OPERAND_NONE:
+		break;
 	case ASM_OPERAND_IMMEDIATE:
-		debug("  IMMEDIATE %lld", operand->num);
+		debug("  IMMEDIATE %lld", operand->u.num);
 		break;
-	case ASM_OPERAND_REGISTER_EAX:
-		debug("  EAX");
+	case ASM_OPERAND_REGISTER:
+		switch (operand->u.reg) {
+		case ASM_REGISTER_AX:
+			debug("  EAX");
+			break;
+		case ASM_REGISTER_R10:
+			debug("  R10");
+			break;
+		case ASM_REGISTER_RSP:
+			debug("  RSP");
+			break;
+		}
 		break;
+	case ASM_OPERAND_PSEUDO_REGISTER:
+		debug("  PSEUDO %lld", operand->u.num);
+		break;
+	case ASM_OPERAND_STACK:
+		debug("  STACK %lld",
+		      -1 * CODEGEN_BYTES_PER_VALUE * operand->u.num);
+		break;
+	}
+}
+
+static void
+codegen_debug_print_op(const struct asm_op *op)
+{
+	bool print_operands = true;
+
+	switch (op->opcode) {
+	case ASM_OP_MOV:
+		debug("MOV");
+		break;
+	case ASM_OP_SUB:
+		debug("SUB");
+		break;
+	case ASM_OP_UNARY_NEG:
+		debug("NEG");
+		break;
+	case ASM_OP_UNARY_NOT:
+		debug("NOT");
+		break;
+	case ASM_OP_RET:
+		debug("RET");
+		print_operands = false;
+		break;
+	}
+
+	for (size_t i = 0; print_operands && i < ARRAY_SIZE(op->args); ++i) {
+		codegen_debug_print_operand(&op->args[i]);
 	}
 }
 
 void
 codegen_debug_print(const struct assembly *cg)
 {
-	if (cg == NULL) {
-		return;
-	}
+	debug("PROGRAM");
 
-	switch (cg->statement_type) {
-	case ASM_PROGRAM: {
-		debug("PROGRAM");
-		const struct asm_program *p = (const struct asm_program *)cg;
-		codegen_debug_print(&p->function.base);
-		break;
-	}
-	case ASM_FUNCTION: {
-		const struct asm_function *f = (const struct asm_function *)cg;
-		const struct string_view *str = &f->identifier;
-		debug("FUNCTION %.*s", (int)str->sz, str->data);
-		codegen_debug_print(&f->ops->base);
-		break;
-	}
-	case ASM_OP_MOV: {
-		debug("MOV");
-		const struct asm_op *ops = (const struct asm_op *)cg;
-		for (size_t i = 0; i < ARRAY_SIZE(ops->args); ++i) {
-			codegen_debug_print_operand(&ops->args[i]);
-		}
-		codegen_debug_print(&ops->next->base);
-		break;
-	}
-	case ASM_OP_RET: {
-		debug("RET");
-		const struct asm_op *ops = (const struct asm_op *)cg;
-		codegen_debug_print(&ops->next->base);
-		break;
-	}
+	const struct string_view *fname = &cg->function.identifier;
+	debug("FUNCTION %.*s", (int)fname->sz, fname->data);
+
+	for (struct asm_op *op = cg->function.ops; op != NULL; op = op->next) {
+		codegen_debug_print_op(op);
 	}
 }
--- src/passes/parse.c
+++ src/passes/parse.c
@@ -10,6 +10,23 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+static WARN_UNUSED result_t
+parse_alloc_helper(struct ast **a, unsigned node_type)
+{
+	assert(a != NULL && *a == NULL);
+	*a = malloc(sizeof(**a));
+	check_if(*a == NULL, ERR_LEX_ALLOC);
+	memset(*a, 0, sizeof(**a));
+	(**a).node_type = node_type;
+	return RESULT_OK;
+}
+
+#define parse_alloc(a, nt)                                                     \
+	do {                                                                   \
+		check(parse_alloc_helper((a), (nt)));                          \
+		assert(*(a)); /* silence clang-analyzer NullDereference */     \
+	} while (0);
+
 static WARN_UNUSED bool
 is_token_type(const struct token *tok, unsigned expected)
 {
@@ -19,17 +36,14 @@ is_token_type(const struct token *tok, unsigned expected)
 static void
 token_consume(const struct token **tok)
 {
-	*tok = (*tok)->next;
+	*tok = (**tok).next;
 }
 
 static WARN_UNUSED result_t
-parse_constant(const struct token **tok, struct ast_constant *dst)
+parse_constant(const struct token **tok, struct ast **dst)
 {
-	dst->base.node_type = NODE_CONSTANT_INT;
-
-	if (!is_token_type(*tok, TOKEN_CONSTANT)) {
-		return make_result(ERR_PARSE_CONSTANT_EXPECT_TOKEN_CONSTANT);
-	}
+	assert(is_token_type(*tok, TOKEN_CONSTANT));
+	parse_alloc(dst, NODE_CONSTANT_INT);
 
 	/*
 	 * strtoll() does not update errno on success, so we must clear it
@@ -37,12 +51,12 @@ parse_constant(const struct token **tok, struct ast_constant *dst)
 	 */
 	errno = 0;
 
-	dst->num = strtoll((*tok)->val.data, NULL, 0);
+	(**dst).u.num = strtoll((**tok).val.data, NULL, 0);
 	if (errno != 0) {
 		return make_result(ERR_PARSE_CONSTANT_STRTOLL,
 		                   errno,
-		                   (*tok)->val.data,
-		                   (*tok)->val.sz);
+		                   (**tok).val.data,
+		                   (**tok).val.sz);
 	}
 
 	token_consume(tok);
@@ -50,24 +64,60 @@ parse_constant(const struct token **tok, struct ast_constant *dst)
 }
 
 static WARN_UNUSED result_t
-parse_expression(const struct token **tok, struct ast_expression *dst)
+parse_identifier(const struct token **tok, struct ast **dst)
 {
-	dst->base.node_type = NODE_EXPRESSION;
-	check(parse_constant(tok, &dst->constant));
+	if (!is_token_type(*tok, TOKEN_IDENTIFIER)) {
+		return make_result(ERR_PARSE_FUNC_NAME_EXPECT_TOKEN_IDENTIFIER);
+	}
+	parse_alloc(dst, NODE_IDENTIFIER);
+
+	(**dst).u.str = (**tok).val;
+	token_consume(tok);
 	return RESULT_OK;
 }
 
 static WARN_UNUSED result_t
-parse_statement(const struct token **tok, struct ast_statement *dst)
+parse_expression(const struct token **tok, struct ast **dst)
 {
-	dst->base.node_type = NODE_STATEMENT;
+	assert(!is_token_type(*tok, TOKEN_HYPHEN_HYPHEN)); // unimplemented
+
+	if (is_token_type(*tok, TOKEN_CONSTANT)) {
+		parse_alloc(dst, NODE_EXPRESSION_UNARY_IDENTITY);
+		check(parse_constant(tok, &(**dst).u.op_unary.operand));
+	} else if (is_token_type(*tok, TOKEN_TILDE)) {
+		parse_alloc(dst, NODE_EXPRESSION_UNARY_COMPLEMENT);
+		token_consume(tok);
+		check(parse_expression(tok, &(**dst).u.op_unary.operand));
+	} else if (is_token_type(*tok, TOKEN_HYPHEN)) {
+		parse_alloc(dst, NODE_EXPRESSION_UNARY_NEGATION);
+		token_consume(tok);
+		check(parse_expression(tok, &(**dst).u.op_unary.operand));
+	} else if (is_token_type(*tok, TOKEN_PAREN_OPEN)) {
+		parse_alloc(dst, NODE_EXPRESSION_PAREN_ENCLOSED);
+		token_consume(tok);
+		check(parse_expression(tok, &(**dst).u.op_unary.operand));
+		if (!is_token_type(*tok, TOKEN_PAREN_CLOSE)) {
+			return make_result(
+				ERR_PARSE_EXPR_EXPECT_TOKEN_PAREN_CLOSE);
+		}
+		token_consume(tok);
+	} else {
+		return make_result(ERR_PARSE_EXPR_EXPECT_REASONABLE);
+	}
+	return RESULT_OK;
+}
+
+static WARN_UNUSED result_t
+parse_statement(const struct token **tok, struct ast **dst)
+{
+	parse_alloc(dst, NODE_EXPRESSION_UNARY_IDENTITY);
 
 	if (!is_token_type(*tok, TOKEN_KEYWORD_RETURN)) {
 		return make_result(ERR_PARSE_STMT_EXPECT_TOKEN_KEYWORD_RETURN);
 	}
 	token_consume(tok);
 
-	check(parse_expression(tok, &dst->return_expression));
+	check(parse_expression(tok, &(**dst).u.op_unary.operand));
 
 	if (!is_token_type(*tok, TOKEN_SEMICOLON)) {
 		return make_result(ERR_PARSE_STMT_EXPECT_TOKEN_SEMICOLON);
@@ -78,21 +128,16 @@ parse_statement(const struct token **tok, struct ast_statement *dst)
 }
 
 static WARN_UNUSED result_t
-parse_function(const struct token **tok, struct ast_function *dst)
+parse_function(const struct token **tok, struct ast **dst)
 {
-	dst->base.node_type = NODE_FUNCTION;
+	parse_alloc(dst, NODE_FUNCTION);
 
 	if (!is_token_type(*tok, TOKEN_KEYWORD_INT)) {
 		return make_result(ERR_PARSE_FUNC_EXPECT_RETURN_TYPE_INT);
 	}
 	token_consume(tok);
 
-	if (!is_token_type(*tok, TOKEN_IDENTIFIER)) {
-		return make_result(ERR_PARSE_FUNC_NAME_EXPECT_TOKEN_IDENTIFIER);
-	}
-	dst->identifier.base.node_type = NODE_IDENTIFIER;
-	dst->identifier.token = (*tok)->val;
-	token_consume(tok);
+	check(parse_identifier(tok, &(**dst).u.function.identifier));
 
 	if (!is_token_type(*tok, TOKEN_PAREN_OPEN)) {
 		return make_result(ERR_PARSE_FUNC_EXPECT_TOKEN_PAREN_OPEN);
@@ -114,7 +159,7 @@ parse_function(const struct token **tok, struct ast_function *dst)
 	}
 	token_consume(tok);
 
-	check(parse_statement(tok, &dst->statement));
+	check(parse_statement(tok, &(**dst).u.function.statement));
 
 	if (!is_token_type(*tok, TOKEN_BRACE_CLOSE)) {
 		return make_result(ERR_PARSE_FUNC_EXPECT_TOKEN_BRACE_CLOSE);
@@ -124,28 +169,16 @@ parse_function(const struct token **tok, struct ast_function *dst)
 	return RESULT_OK;
 }
 
-static WARN_UNUSED result_t
-parse_program(const struct token **tok, struct ast_program *dst)
-{
-	dst->base.node_type = NODE_PROGRAM;
-	check(parse_function(tok, &dst->function));
-	return RESULT_OK;
-}
-
 result_t
 parse_init(const struct token *tok, struct ast **a)
 {
-	struct ast_program program = {0};
-	check(parse_program(&tok, &program));
+	parse_alloc(a, NODE_PROGRAM);
+
+	check(parse_function(&tok, &(**a).u.program.entrypoint_function));
 	if (tok != NULL) {
 		return make_result(ERR_PARSE_PROG_EXPECT_END);
 	}
 
-	struct ast_program *out = malloc(sizeof(*out));
-	check_if(out == NULL, ERR_PARSE_ALLOC);
-	memcpy(out, &program, sizeof(*out));
-
-	*a = &out->base;
 	return RESULT_OK;
 }
 
@@ -165,42 +198,41 @@ void
 parse_debug_print(const struct ast *a, size_t indent)
 {
 	assert(indent <= INT_MAX);
-	const struct string_view *s = NULL;
 	switch (a->node_type) {
 	case NODE_PROGRAM:
 		debug("%*sPROGRAM", (int)indent, "");
-		parse_debug_print(&((struct ast_program *)a)->function.base,
-		                  indent + 1);
+		parse_debug_print(a->u.program.entrypoint_function, indent + 1);
 		break;
 	case NODE_FUNCTION:
 		debug("%*sFUNCTION", (int)indent, "");
 		debug("%*sNAME", (int)(indent + 1), "");
-		parse_debug_print(&((struct ast_function *)a)->identifier.base,
-		                  indent + 2);
+		parse_debug_print(a->u.function.identifier, indent + 2);
 		debug("%*sBODY", (int)(indent + 1), "");
-		parse_debug_print(&((struct ast_function *)a)->statement.base,
-		                  indent + 2);
+		parse_debug_print(a->u.function.statement, indent + 2);
 		break;
-	case NODE_STATEMENT:
-		debug("%*sSTATEMENT", (int)indent, "");
-		parse_debug_print(
-			&((struct ast_statement *)a)->return_expression.base,
-			indent + 1);
+	case NODE_EXPRESSION_UNARY_IDENTITY:
+		debug("%*sEXPRESSION IDENTITY", (int)indent, "");
+		parse_debug_print(a->u.op_unary.operand, indent + 1);
 		break;
-	case NODE_EXPRESSION:
-		debug("%*sEXPRESSION", (int)indent, "");
-		parse_debug_print(&((struct ast_expression *)a)->constant.base,
-		                  indent + 1);
+	case NODE_EXPRESSION_UNARY_NEGATION:
+		debug("%*sEXPRESSION NEGATION", (int)indent, "");
+		parse_debug_print(a->u.op_unary.operand, indent + 1);
 		break;
-	case NODE_IDENTIFIER:
-		s = &((struct ast_identifier *)a)->token;
+	case NODE_EXPRESSION_UNARY_COMPLEMENT:
+		debug("%*sEXPRESSION COMPLEMENT", (int)indent, "");
+		parse_debug_print(a->u.op_unary.operand, indent + 1);
+		break;
+	case NODE_EXPRESSION_PAREN_ENCLOSED:
+		debug("%*sEXPRESSION PARENTHESIZED", (int)indent, "");
+		parse_debug_print(a->u.op_unary.operand, indent + 1);
+		break;
+	case NODE_IDENTIFIER: {
+		const struct string_view *s = &a->u.str;
 		debug("%*sIDENT %.*s", (int)indent, "", (int)s->sz, s->data);
 		break;
+	}
 	case NODE_CONSTANT_INT:
-		debug("%*sCONSTANT %lld",
-		      (int)indent,
-		      "",
-		      ((struct ast_constant *)a)->num);
+		debug("%*sCONSTANT %lld", (int)indent, "", a->u.num);
 		break;
 	}
 }
