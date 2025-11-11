628f08553b53c0913af7d9d651b27ef4c93c8a87
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-11-10 20:37:33 -0800

    diff reduction relative to c43722fa

--- src/passes/codegen.c
+++ src/passes/codegen.c
@@ -129,8 +129,8 @@ codegen_statement_one(const struct ir_op *src, struct asm_op **dst)
 		codegen_alloc(*dst);
 		(**dst).opcode = ASM_OP_RET;
 		break;
-	case IR_OP_UNARY_COMPLEMENT:
 	case IR_OP_UNARY_NEGATE:
+	case IR_OP_UNARY_COMPLEMENT:
 		(**dst).opcode = ASM_OP_MOV;
 		for (size_t i = 0; i < ARRAY_SIZE((**dst).args); ++i) {
 			codegen_map_operand(&src->args[i], &(**dst).args[i]);
