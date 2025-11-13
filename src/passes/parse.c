492b8f8c8fef721be5926904ece4736afd33a9a8
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-11-13 03:01:48 -0800

    implement NODE_IF_ELSE case in resolve_expr()

--- src/passes/parse.c
+++ src/passes/parse.c
@@ -43,8 +43,16 @@ resolve_expr(Arena *arena, struct ast *a, struct symbol **sym)
 	case NODE_FUNCTION:
 	case NODE_BLOCK:
 	case NODE_DECLARATION:
-	case NODE_IF_ELSE:
 		assert(0); /* logic error in caller */
+		break;
+	case NODE_IF_ELSE:
+		check(resolve_expr(arena, a->u.if_.condition, sym));
+		check(resolve_expr(arena, a->u.if_.then_clause, sym));
+		if (a->u.if_.else_clause != NULL) {
+			check(resolve_expr(arena,
+			                   a->u.if_.else_clause,
+			                   sym));
+		}
 		break;
 	case NODE_EXPRESSION_NULL:
 	case NODE_CONSTANT_INT:
