00c1ee317aa8f1f20f613aa431eccdab7cb50587
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-11-13 20:28:24 -0800

    fix segfault on NULL block in parse_debug_print()

--- src/passes/parse.c
+++ src/passes/parse.c
@@ -633,8 +633,8 @@ parse_debug_print(const struct ast *a, size_t indent)
 		break;
 	case NODE_BLOCK:
 		debug("%*sBLOCK ITEM", (int)indent, "");
-		parse_debug_print(a->u.block.item, indent + 1);
-		if (a->u.block.next != NULL) {
+		if (a->u.block.item != NULL) {
+			parse_debug_print(a->u.block.item, indent + 1);
 			parse_debug_print(a->u.block.next, indent);
 		}
 		break;
