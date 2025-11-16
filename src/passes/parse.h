3aa2d87efe7019f994e7f6328ce225c1efde0b3c
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-11-15 23:53:41 -0800

    ch 9: functions

--- src/passes/parse.h
+++ src/passes/parse.h
@@ -1,54 +1,65 @@
 #ifndef COMPILER_PASSES_PARSE_H
 #define COMPILER_PASSES_PARSE_H
 
+#include "passes/symbol.h"
 #include "sys/string_view.h"
 
 struct ast_symbol {
 	struct string_view name;
 	long long int unique;
+	enum symbol_type stype;
 };
 
+#define FOREACH_AST_NODETYPE(F)                                                \
+	F(PROGRAM)                                                             \
+	F(FUNCTION)                                                            \
+	F(FUNCTION_RETURN_STATEMENT)                                           \
+	F(BLOCK)                                                               \
+	F(DECLARATION)                                                         \
+	F(IF_ELSE)                                                             \
+	F(LOOP)                                                                \
+	F(BREAK)                                                               \
+	F(CONTINUE)                                                            \
+	F(EXPRESSION_NULL)                                                     \
+	F(EXPRESSION_UNARY_COMPLEMENT)                                         \
+	F(EXPRESSION_UNARY_NEGATE)                                             \
+	F(EXPRESSION_UNARY_NOT)                                                \
+	F(EXPRESSION_PAREN_ENCLOSED)                                           \
+	F(EXPRESSION_BINARY_ADD)                                               \
+	F(EXPRESSION_BINARY_SUBTRACT)                                          \
+	F(EXPRESSION_BINARY_MULTIPLY)                                          \
+	F(EXPRESSION_BINARY_DIVIDE)                                            \
+	F(EXPRESSION_BINARY_REMAINDER)                                         \
+	F(EXPRESSION_LOGICAL_AND)                                              \
+	F(EXPRESSION_LOGICAL_OR)                                               \
+	F(EXPRESSION_COMPARE_EQUAL)                                            \
+	F(EXPRESSION_COMPARE_NOT_EQUAL)                                        \
+	F(EXPRESSION_COMPARE_LESS_THAN)                                        \
+	F(EXPRESSION_COMPARE_LESS_THAN_EQ)                                     \
+	F(EXPRESSION_COMPARE_MORE_THAN)                                        \
+	F(EXPRESSION_COMPARE_MORE_THAN_EQ)                                     \
+	F(EXPRESSION_VARIABLE_ASSIGNMENT)                                      \
+	F(EXPRESSION_VARIABLE_USAGE)                                           \
+	F(EXPRESSION_TERNARY_CONDITIONAL)                                      \
+	F(EXPRESSION_FUNCTION_CALL)                                            \
+	F(EXPRESSION_FUNCTION_CALL_ARGUMENTS)                                  \
+	F(CONSTANT_INT)
+
+#define TO_ENUM(node_type) NODE_##node_type,
+enum ast_nodetype { FOREACH_AST_NODETYPE(TO_ENUM) };
+#undef TO_ENUM
+
 struct ast {
-	enum {
-		NODE_PROGRAM,
-		NODE_FUNCTION,
-		NODE_FUNCTION_RETURN_STATEMENT,
-		NODE_BLOCK,
-		NODE_DECLARATION,
-		NODE_IF_ELSE,
-		NODE_LOOP,
-		NODE_BREAK,
-		NODE_CONTINUE,
-		NODE_EXPRESSION_NULL,
-		NODE_EXPRESSION_UNARY_COMPLEMENT,
-		NODE_EXPRESSION_UNARY_NEGATE,
-		NODE_EXPRESSION_UNARY_NOT,
-		NODE_EXPRESSION_PAREN_ENCLOSED,
-		NODE_EXPRESSION_BINARY_ADD,
-		NODE_EXPRESSION_BINARY_SUBTRACT,
-		NODE_EXPRESSION_BINARY_MULTIPLY,
-		NODE_EXPRESSION_BINARY_DIVIDE,
-		NODE_EXPRESSION_BINARY_REMAINDER,
-		NODE_EXPRESSION_LOGICAL_AND,
-		NODE_EXPRESSION_LOGICAL_OR,
-		NODE_EXPRESSION_COMPARE_EQUAL,
-		NODE_EXPRESSION_COMPARE_NOT_EQUAL,
-		NODE_EXPRESSION_COMPARE_LESS_THAN,
-		NODE_EXPRESSION_COMPARE_LESS_THAN_EQ,
-		NODE_EXPRESSION_COMPARE_MORE_THAN,
-		NODE_EXPRESSION_COMPARE_MORE_THAN_EQ,
-		NODE_EXPRESSION_VARIABLE_USAGE,
-		NODE_EXPRESSION_VARIABLE_ASSIGNMENT,
-		NODE_EXPRESSION_TERNARY_CONDITIONAL,
-		NODE_CONSTANT_INT,
-	} node_type;
+	enum ast_nodetype node_type;
 	union {
 		struct {
-			struct ast *entrypoint_function;
+			struct ast *globals;
 		} program;
 		struct {
 			struct ast_symbol identifier;
+			struct ast_symbol *params;
 			struct ast *block;
+			struct ast *next;
 		} function;
 		struct {
 			struct ast *item;
@@ -84,9 +95,27 @@ struct ast {
 			struct ast *then_expr;
 			struct ast *else_expr;
 		} op_ternary;
+		struct {
+			struct ast_symbol identifier;
+			struct ast *arguments;
+		} call;
+		struct {
+			struct ast *expr;
+			struct ast *next;
+		} call_args;
 		struct ast_symbol var; /* NODE_EXPRESSION_VARIABLE_USAGE */
 		long long int num;     /* NODE_CONSTANT_INT */
 	} u;
 };
+
+/*
+ * Utility macro for iterating over dynamically allocated u.function.params
+ * array, delimited by a final `struct string_view` with NULL data.
+ */
+#define FOREACH_FUNCTION_PARAMETER(iter, arr)                                  \
+	for (struct ast_symbol * (iter) = arr;                                 \
+	     (iter) != NULL && (iter)->name.data != NULL &&                    \
+	     (iter)->name.sz > 0;                                              \
+	     ++(iter))
 
 #endif
