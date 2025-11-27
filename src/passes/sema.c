b7aa26914208fd730e87b6d420ba805c0c981bcc
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-11-26 20:49:32 -0800

    make sema check postincrement/preincrement like variable assignment, then loosen the check to accept N levels of parenthesized expressions around variable usage, in addition to plain variable usage

--- src/passes/sema.c
+++ src/passes/sema.c
@@ -9,13 +9,12 @@
 static WARN_UNUSED result_t
 sema_walk(struct ast *a, result_t (*f)(struct ast *a, void *userdata), void *u)
 {
+	check(f(a, u));
 	switch (a->node_type) {
 	case NODE_PROGRAM:
-		check(f(a, u));
 		check(sema_walk(a->u.program.globals, f, u));
 		break;
 	case NODE_FUNCTION:
-		check(f(a, u));
 		if (a->u.function.block != NULL) {
 			check(sema_walk(a->u.function.block, f, u));
 		}
@@ -32,7 +31,6 @@ sema_walk(struct ast *a, result_t (*f)(struct ast *a, void *userdata), void *u)
 		}
 		break;
 	case NODE_DECLARATION:
-		check(f(a, u));
 		if (a->u.declare.init != NULL) {
 			check(sema_walk(a->u.declare.init, f, u));
 		}
@@ -48,17 +46,13 @@ sema_walk(struct ast *a, result_t (*f)(struct ast *a, void *userdata), void *u)
 		}
 		break;
 	case NODE_LOOP:
-		check(f(a, u));
 		check(sema_walk(a->u.loop.precond, f, u));
 		check(sema_walk(a->u.loop.body, f, u));
 		check(sema_walk(a->u.loop.incr, f, u));
 		check(sema_walk(a->u.loop.postcond, f, u));
 		break;
 	case NODE_BREAK:
-		check(f(a, u));
-		break;
 	case NODE_CONTINUE:
-		check(f(a, u));
 		break;
 	case NODE_FUNCTION_RETURN_STATEMENT:
 	case NODE_EXPRESSION_UNARY_NEGATE:
@@ -72,8 +66,6 @@ sema_walk(struct ast *a, result_t (*f)(struct ast *a, void *userdata), void *u)
 		check(sema_walk(a->u.op_unary.operand, f, u));
 		break;
 	case NODE_EXPRESSION_VARIABLE_ASSIGNMENT:
-		check(f(a, u));
-		__attribute__((fallthrough));
 	case NODE_EXPRESSION_BINARY_ADD:
 	case NODE_EXPRESSION_BINARY_SUBTRACT:
 	case NODE_EXPRESSION_BINARY_MULTIPLY:
@@ -105,9 +97,6 @@ sema_walk(struct ast *a, result_t (*f)(struct ast *a, void *userdata), void *u)
 		check(sema_walk(a->u.op_binary.lhs, f, u));
 		check(sema_walk(a->u.op_binary.rhs, f, u));
 		break;
-	case NODE_EXPRESSION_VARIABLE_USAGE:
-		check(f(a, u));
-		break;
 	case NODE_EXPRESSION_TERNARY_CONDITIONAL:
 		check(sema_walk(a->u.op_binary.lhs, f, u));
 		check(sema_walk(a->u.op_ternary.condition, f, u));
@@ -117,7 +106,6 @@ sema_walk(struct ast *a, result_t (*f)(struct ast *a, void *userdata), void *u)
 		}
 		break;
 	case NODE_EXPRESSION_FUNCTION_CALL:
-		check(f(a, u));
 		if (a->u.call.arguments != NULL) {
 			check(sema_walk(a->u.call.arguments, f, u));
 		}
@@ -128,6 +116,7 @@ sema_walk(struct ast *a, result_t (*f)(struct ast *a, void *userdata), void *u)
 			check(sema_walk(a->u.call_args.next, f, u));
 		}
 		break;
+	case NODE_EXPRESSION_VARIABLE_USAGE:
 	case NODE_EXPRESSION_NULL:
 	case NODE_CONSTANT_INT:
 		break;
@@ -176,14 +165,33 @@ sema_label_loops(struct ast *a, long long int *generator)
 static WARN_UNUSED result_t
 sema_lvalue(struct ast *a, void *userdata MAYBE_UNUSED)
 {
-	if (a->node_type == NODE_EXPRESSION_VARIABLE_ASSIGNMENT &&
-	    a->u.op_binary.lhs->node_type != NODE_EXPRESSION_VARIABLE_USAGE) {
+	struct ast *to_check = NULL;
+	switch (a->node_type) {
+	case NODE_EXPRESSION_VARIABLE_ASSIGNMENT:
+		to_check = a->u.op_binary.lhs;
+		break;
+	case NODE_EXPRESSION_PREDECREMENT:
+	case NODE_EXPRESSION_POSTDECREMENT:
+	case NODE_EXPRESSION_PREINCREMENT:
+	case NODE_EXPRESSION_POSTINCREMENT:
+		to_check = a->u.op_unary.operand;
+		break;
+	default:
+		return RESULT_OK;
+	}
+
+	while (to_check->node_type == NODE_EXPRESSION_PAREN_ENCLOSED) {
+		to_check = to_check->u.op_unary.operand;
+	}
+
+	if (to_check->node_type != NODE_EXPRESSION_VARIABLE_USAGE) {
 		/*
 		 * See related assertions in src/passes/ir.c on u.op_binary.lhs
 		 * and NODE_EXPRESSION_VARIABLE_USAGE.
 		 */
 		return make_result(ERR_SEMA_VARIABLE_DECLARATION_BAD_LVALUE);
 	}
+
 	return RESULT_OK;
 }
 
