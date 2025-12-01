dccea63fd0551518cf8ce85c029f4df95b1ff37c
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-11-30 16:24:39 -0800

    revert f4b8902, which regressed other tests

--- src/passes/sema.c
+++ src/passes/sema.c
@@ -518,9 +518,6 @@ sema_lvalue(struct ast *a, void *userdata MAYBE_UNUSED)
 	case NODE_EXPRESSION_POSTINCREMENT:
 		to_check = a->u.op_unary.operand;
 		break;
-	case NODE_EXPRESSION_CAST:
-		to_check = a->u.cast.expr;
-		break;
 	default:
 		return RESULT_OK;
 	}

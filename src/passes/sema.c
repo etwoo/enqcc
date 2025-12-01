etwoo/enqcc

commit f4b89021017602e269605669f6f550b09e36837d

reject cast on lvalue; triggered by cast_lvalue.c

src/passes/sema.c
@@ -518,6 +518,9 @@ sema_lvalue(struct ast *a, void *userdata MAYBE_UNUSED)
 	case NODE_EXPRESSION_POSTINCREMENT:
 		to_check = a->u.op_unary.operand;
 		break;
+	case NODE_EXPRESSION_CAST:
+		to_check = a->u.cast.expr;
+		break;
 	default:
 		return RESULT_OK;
 	}

commit dccea63fd0551518cf8ce85c029f4df95b1ff37c

revert f4b8902, which regressed other tests

src/passes/sema.c
@@ -518,9 +518,6 @@ sema_lvalue(struct ast *a, void *userdata MAYBE_UNUSED)
 	case NODE_EXPRESSION_POSTINCREMENT:
 		to_check = a->u.op_unary.operand;
 		break;
-	case NODE_EXPRESSION_CAST:
-		to_check = a->u.cast.expr;
-		break;
 	default:
 		return RESULT_OK;
 	}