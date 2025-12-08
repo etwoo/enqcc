4a9c99eb1b1a1b9d9f87d2e832fdd7d9e9440cc9
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-12-08 04:12:52 +0000

    workaround linux issues

--- cmake/CheckHardened.cmake
+++ cmake/CheckHardened.cmake
@@ -28,7 +28,6 @@ macro(check_hardened lib)
 		_my_msg_hardened(" - Failed")
 		# -fhardened unsupported; set constituent options individually
 		target_compile_options(${lib} INTERFACE
-			-D_FORTIFY_SOURCE=3
 			-D_GLIBCXX_ASSERTIONS
 			-fPIE -pie
 			-Wl,-z,now
@@ -47,5 +46,8 @@ macro(check_hardened lib)
 	#
 	check_target_compile_options(${lib} -mbranch-protection=standard)
 
-	target_compile_options(${lib} INTERFACE -O2) # for _FORTIFY_SOURCE
+	#
+	# disabled: -D_FORTIFY_SOURCE=3 -O2
+	# XXX, arena.h segfaults: https://github.com/tsoding/arena/issues/3
+	#
 endmacro()
