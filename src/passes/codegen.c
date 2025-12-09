15e56cf3e1514fc8d0007392e7495252478a8cdd
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-12-08 16:36:39 -0800

    fix d974ea0, meant to change uint->double, not double->uint; trying to fix unsigned_to_double.c

--- src/passes/codegen.c
+++ src/passes/codegen.c
@@ -710,11 +710,6 @@ codegen_statement_one(Arena *arena,
 		 * orq       %rcx, %rax
 		 * movq      %rax, -16(%rbp)    ; result at -16(%rbp)
 		 */
-		(**dst).opcode = ASM_OP_BITWISE_XOR;
-		(**dst).args[0] = OPERAND_XMM0; /* zero out XMM0, in case of */
-		(**dst).args[1] = OPERAND_XMM0; /* 32-bit arg (long vs quad) */
-		dst = &(**dst).next;
-		check(codegen_alloc_op(arena, dst));
 		(**dst).opcode = ASM_OP_MOV;
 		codegen_map_operand(&src->args[0], &(**dst).args[0]);
 		(**dst).args[1] = OPERAND_XMM0;
@@ -789,6 +784,11 @@ codegen_statement_one(Arena *arena,
 		 *
 		 * See block comment on IR_OP_CTYPE_DOUBLE_TO_UINT as well.
 		 */
+		(**dst).opcode = ASM_OP_DOUBLE_BITWISE_XOR;
+		(**dst).args[0] = OPERAND_XMM0; /* zero out XMM0, in case of */
+		(**dst).args[1] = OPERAND_XMM0; /* 32-bit arg (long vs quad) */
+		dst = &(**dst).next;
+		check(codegen_alloc_op(arena, dst));
 		(**dst).opcode = ASM_OP_MOV;
 		codegen_map_operand(&src->args[0], &(**dst).args[0]);
 		(**dst).args[1] = OPERAND_XMM0;
