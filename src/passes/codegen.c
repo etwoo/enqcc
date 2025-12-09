e6c89b54292bf946ae35f60120a8ee1b99b953fe
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-12-08 23:37:35 -0800

    define constants to avoid NOLINT warnings on uint->double magic numbers

--- src/passes/codegen.c
+++ src/passes/codegen.c
@@ -21,6 +21,8 @@ static const long long int CODEGEN_FP_REGISTER_ARGS = ARRAY_SIZE(CALL_FP);
 static const long long int CODEGEN_BYTES_PER_VALUE = 4;
 static const long long int CODEGEN_BYTES_PER_PUSH = 8;
 static const long long int CODEGEN_BYTES_ARG_FIRST = 16;
+static const long long unsigned UINT_TO_DBL_MAGIC1 = 0x43300000;
+static const long long unsigned UINT_TO_DBL_MAGIC2 = 0x45300000;
 
 static WARN_UNUSED result_t
 codegen_alloc_op(Arena *arena, struct asm_op **dst)
@@ -652,19 +654,17 @@ codegen_statement_fp(Arena *arena, const struct ir_op *src, struct asm_op **dst)
 			(**dst).opcode = ASM_OP_VEC_DOUBLE_UNPACK_INTERLEAVE_LO;
 			(**dst).args[0].operand_type =
 				ASM_OPERAND_CONSTANT_DATA_VEC_LONGS;
-			(**dst).args[0].u.longs[0] = 0x43300000; // NOLINT
-			(**dst).args[0].u.longs[1] = 0x45300000; // NOLINT
+			(**dst).args[0].u.longs[0] = UINT_TO_DBL_MAGIC1;
+			(**dst).args[0].u.longs[1] = UINT_TO_DBL_MAGIC2;
 			(**dst).args[1] = OPERAND_XMM0;
 			dst = &(**dst).next;
 			check(codegen_alloc_op(arena, dst));
 			(**dst).opcode = ASM_OP_VEC_DOUBLE_BINARY_SUBTRACT;
 			(**dst).args[0].operand_type =
 				ASM_OPERAND_CONSTANT_DATA_VEC_QUADS;
-			// NOLINTBEGIN // TODO rm
-			(**dst).args[0].u.quads[0] = 0x4330000000000000;
-			(**dst).args[0].u.quads[1] = 0x4530000000000000;
+			(**dst).args[0].u.quads[0] = UINT_TO_DBL_MAGIC1 << 8U;
+			(**dst).args[0].u.quads[1] = UINT_TO_DBL_MAGIC2 << 8U;
 			(**dst).args[1] = OPERAND_XMM0;
-			// NOLINTEND // TODO rm
 			dst = &(**dst).next;
 			check(codegen_alloc_op(arena, dst));
 			(**dst).opcode = ASM_OP_MOV;
