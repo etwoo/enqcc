0731b1d439365a1c440b3e85d589eaf4f584ae69
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-12-12 23:40:07 -0800

    rm hack in emit_asm_op() for ASM_OPERAND_VARIABLE_DATA; no longer seems to affect test results, failures remain at 4

--- src/passes/emit.c
+++ src/passes/emit.c
@@ -253,15 +253,11 @@ emit_asm_op(const struct asm_op *op, enum platform plat, int fd)
 		if (op->args[i].operand_type == ASM_OPERAND_NONE) {
 			continue;
 		}
-		if ((i == 0 && /* for VARIABLE_DATA case, restricting hack to
-		                  VARIABLE_DATA as src arg, not dst, seems to
-		                  fix declarators.c arg; TODO; remove? */
-		     op->args[i].operand_type == ASM_OPERAND_VARIABLE_DATA) ||
-		    (op->args[i].operand_type == ASM_OPERAND_MEMORY &&
-		     // TODO: why is this only needed for non-stack pointers?
-		     // and why does this cause failures if i also apply it to
-		     // stack offsets?
-		     op->args[i].u.mem.reg != ASM_REGISTER_RBP)) {
+		if (op->args[i].operand_type == ASM_OPERAND_MEMORY &&
+		    // TODO: why is this only needed for non-stack pointers?
+		    // and why does this cause failures if i also apply it to
+		    // stack offsets?
+		    op->args[i].u.mem.reg != ASM_REGISTER_RBP) {
 			if (op->args[i].word_type ==
 			    ASM_WORD_POINTER_TO_32BIT) {
 				ralias_default = REGISTER_ALIAS_4BYTE;
