95d8b65e885060e0d8e30e9a9c7b5f153f7eabbb
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-12-13 16:46:59 -0800

    fix clang-format whitespace error in result.c

--- src/result.c
+++ src/result.c
@@ -379,8 +379,8 @@ result_to_str(result_t r)
 		s = strdup(
 			"Complement ~, remainder %, and bitwise operations "
 			"cannot take arguments of type 'double'; also, values "
-		        "of type 'double' cannot be cast to pointer type, and "
-		        "pointer values cannot be cast to type 'double'");
+			"of type 'double' cannot be cast to pointer type, and "
+			"pointer values cannot be cast to type 'double'");
 		break;
 	case ERR_SEMA_OPERAND_POINTER_CONFLICT:
 		s = strdup("Conflicting pointer types");
