fadc0373eed788df9f8d811ee64b2498fd942cfe
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-12-14 23:57:52 -0800

    reject empty compound initializer in parse; fixes empty_initializer_list.c, though i would probably prefer to reject this in sema.c under --validate, instead of under --parse

--- src/result.c
+++ src/result.c
@@ -227,6 +227,9 @@ result_to_str(result_t r)
 		           "function declarations, suggesting use of function "
 		           "pointers, which are not supported");
 		break;
+	case ERR_PARSE_DECL_EMPTY_COMPOUND_INITIALIZER:
+		s = strdup("Empty compound initializer is a C23 extension");
+		break;
 	case ERR_PARSE_DECL_EXPECT_BRACE_CLOSE:
 		s = strdup("Parsing compound initializer expects "
 		           "TOKEN_BRACE_CLOSE after TOKEN_BRACE_OPEN and "
