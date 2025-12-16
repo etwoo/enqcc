7dd316a4fa851984673dd68a7ebb35383c7ffefe
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-12-16 01:02:29 -0800

    rough draft: zero-pad if compound initializer specifies fewer elements than associated array declaration

--- src/passes/sema.c
+++ src/passes/sema.c
@@ -940,6 +940,7 @@ sema_expr_types_initializer(Arena *arena,
 	init->expr_type.t = CTYPE_ARRAY_OF;
 
 	long long unsigned element_count = 0;
+	struct flat *pos = NULL;
 
 	/*
 	 * Recurse into compound initializer elements.
@@ -950,12 +951,28 @@ sema_expr_types_initializer(Arena *arena,
 		                                  declaration_type->referent,
 		                                  f->car));
 		++element_count;
+		pos = f; /* record latest non-NULL element */
 	}
 
 	if (element_count > init->expr_type.sz) {
 		return make_result(ERR_SEMA_INIT_COMPOUND_EXCESS_ELEMENTS,
 		                   varname->name.data,
 		                   varname->name.sz);
+	}
+
+	assert(pos != NULL);
+
+	/*
+	 * Pad compound initializer with zeros as necessary.
+	 */
+	for (; element_count < init->expr_type.sz; ++element_count) {
+		assert(pos->cdr == NULL);
+		check(flat_alloc(arena, &pos->cdr));
+		check(parse_alloc(arena, &pos->cdr->car, NODE_CONSTANT));
+		pos->cdr->car->u.num = 0;
+		pos->cdr->car->expr_type.t = CTYPE_INT;
+		pos->cdr->car->expr_type.maybe_null_pointer_constant = true;
+		pos = pos->cdr;
 	}
 
 	return RESULT_OK;
