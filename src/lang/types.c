a53511a578c4bb3367de9fa7197d6e9c462e3708
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-12-18 02:19:51 -0800

    restore array size comparison as part of ctype_is_equal(); fixes assign_incompatible_pointer_types.c

--- src/lang/types.c
+++ src/lang/types.c
@@ -173,6 +173,9 @@ get_common_ctype(const struct ctype *lhs, const struct ctype *rhs)
 bool
 ctype_is_equal(const struct ctype *lhs, const struct ctype *rhs)
 {
+	if (ctype_is_array(lhs) && ctype_is_array(rhs) && lhs->sz != rhs->sz) {
+		return false;
+	}
 	if ((ctype_is_array(lhs) && ctype_is_pointer(rhs)) ||
 	    (ctype_is_pointer(lhs) && ctype_is_array(rhs))) {
 		// TODO: implement pointer decay differently?
