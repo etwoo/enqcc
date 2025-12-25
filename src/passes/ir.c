b85de9d82fa97fdbd3386d460e0cbb0c1c7a191b
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-12-25 00:12:17 -0800

    infer need for IR_OP_GET_ADDRESS when performing pointer path on array variables in ir_ptr_math() and ir_ptr_ptr_math(); fixes tests/chapter_15/valid/subscripting/simple.c

--- src/passes/ir.c
+++ src/passes/ir.c
@@ -1015,6 +1015,34 @@ ir_unary_op(Arena *arena,
 }
 
 static WARN_UNUSED result_t
+ir_expr_get_addr(Arena *arena,
+                 const struct ast *a,
+                 struct intermediate *ir,
+                 struct ir_op **dst,
+                 struct ir_val *return_value)
+{
+	assert(dst != NULL);
+
+	check(ir_expr(arena, a, ir, dst, return_value));
+	assert(return_value->subtype != IR_VAL_NONE);
+
+	if (!ctype_is_array(&return_value->c89type)) {
+		return RESULT_OK;
+	}
+
+	struct ir_op *get_addr = NULL;
+	check(ir_alloc_op(arena, &get_addr));
+	get_addr->opcode = IR_OP_GET_ADDRESS;
+
+	ir_val_copy(return_value, &get_addr->args[0]);
+	check(ir_val_tmpvar_gen(arena, ir, &a->expr_type, &get_addr->args[1]));
+	ir_val_copy(&get_addr->args[1], return_value);
+
+	*dst = ir_op_list_concat(*dst, get_addr);
+	return RESULT_OK;
+}
+
+static WARN_UNUSED result_t
 ir_ptr_ptr_math(Arena *arena,
                 const struct ast *a,
                 struct intermediate *ir,
@@ -1028,15 +1056,19 @@ ir_ptr_ptr_math(Arena *arena,
 
 	struct ir_op *left = NULL;
 	struct ir_val left_return = {0};
-	check(ir_expr(arena, a->u.op_binary.lhs, ir, &left, &left_return));
-	assert(left_return.subtype != IR_VAL_NONE);
-	// TODO: if left is array type, automatically add IR_OP_GET_ADDRESS
+	check(ir_expr_get_addr(arena,
+	                       a->u.op_binary.lhs,
+	                       ir,
+	                       &left,
+	                       &left_return));
 
 	struct ir_op *right = NULL;
 	struct ir_val right_return = {0};
-	check(ir_expr(arena, a->u.op_binary.rhs, ir, &right, &right_return));
-	assert(right_return.subtype != IR_VAL_NONE);
-	// TODO: if right is array type, automatically add IR_OP_GET_ADDRESS
+	check(ir_expr_get_addr(arena,
+	                       a->u.op_binary.rhs,
+	                       ir,
+	                       &right,
+	                       &right_return));
 
 	struct ir_op *binary = NULL;
 	check(ir_alloc_op(arena, &binary));
@@ -1099,15 +1131,19 @@ ir_ptr_math(Arena *arena,
 
 	struct ir_op *left = NULL;
 	struct ir_val left_return = {0};
-	check(ir_expr(arena, a->u.op_binary.lhs, ir, &left, &left_return));
-	assert(left_return.subtype != IR_VAL_NONE);
-	// TODO: if left is array type, automatically add IR_OP_GET_ADDRESS
+	check(ir_expr_get_addr(arena,
+	                       a->u.op_binary.lhs,
+	                       ir,
+	                       &left,
+	                       &left_return));
 
 	struct ir_op *right = NULL;
 	struct ir_val right_return = {0};
-	check(ir_expr(arena, a->u.op_binary.rhs, ir, &right, &right_return));
-	assert(right_return.subtype != IR_VAL_NONE);
-	// TODO: if right is array type, automatically add IR_OP_GET_ADDRESS
+	check(ir_expr_get_addr(arena,
+	                       a->u.op_binary.rhs,
+	                       ir,
+	                       &right,
+	                       &right_return));
 
 	if (negate_rhs) {
 		assert(!swap_lhs_rhs);
