6f93655f0105f2c340d19de533ad0bd8aa64fc23
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-12-30 18:57:13 -0800

    handle int->char truncation for static initializer of char variables with linkage, similar to long->int truncation

--- src/passes/sema.c
+++ src/passes/sema.c
@@ -86,6 +86,7 @@ count_initializer_elements(const struct ast *a)
 	return count;
 }
 
+static const long long int INT_TO_CHAR_TRUNCATOR = 256;
 static const long long int LONG_TO_INT_TRUNCATOR = 4294967296;
 
 static void
@@ -139,6 +140,12 @@ map_numeric_type_scalar(const struct ast *a,
 	case CTYPE_ARRAY_OF:
 		assert(0); /* logic error in caller */
 		break;
+	}
+
+	if ((dst_type->t == CTYPE_CHAR && x > CHAR_MAX) ||
+	    (dst_type->t == CTYPE_SIGNED_CHAR && x > SCHAR_MAX) ||
+	    (dst_type->t == CTYPE_UNSIGNED_CHAR && x > UCHAR_MAX)) {
+		x %= INT_TO_CHAR_TRUNCATOR;
 	}
 
 	if ((dst_type->t == CTYPE_INT && x > INT_MAX) ||
