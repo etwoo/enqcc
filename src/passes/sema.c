592cb46e8747b4e69eab5f68018ecb0fe3f10eb8
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2026-01-08 01:34:53 -0800

    refine where implicit conversion beten void* and other pointer types occurs, e.g. to apply to ==, != but not to <, <=, >, >=; fixes compare_void_to_other_pointer.c

--- src/passes/sema.c
+++ src/passes/sema.c
@@ -44,7 +44,7 @@ is_node_lvalue(const struct ast *a)
 	}
 	return a->node_type == NODE_EXPRESSION_VARIABLE_USAGE ||
 	       (a->node_type == NODE_EXPRESSION_UNARY_DEREFERENCE &&
-		!ctype_is_void_ptr(&a->u.op_unary.operand->expr_type));
+	        !ctype_is_void_ptr(&a->u.op_unary.operand->expr_type));
 }
 
 static WARN_UNUSED const struct ast *
@@ -1720,11 +1720,17 @@ sema_double(struct ast *a, void *userdata MAYBE_UNUSED)
 	return RESULT_OK;
 }
 
+static const uint32_t SPC_ZERO_AS_NULL = 0x1;
+static const uint32_t SPC_VOIDP_AS_ANY_PTR = 0x2;
+
 static WARN_UNUSED result_t
 sema_pointer_cmp_impl(const struct ctype *lhs,
                       const struct ctype *rhs,
-                      bool ish)
+                      uint32_t flags)
 {
+	const bool ish = (0 != (flags & SPC_ZERO_AS_NULL));
+	const bool void_ptr_as_any_ptr = (0 != (flags & SPC_VOIDP_AS_ANY_PTR));
+
 	if (ctype_is_equal(lhs, rhs)) {
 		/* given equality, nothing more to check */
 	} else if (ctype_is_strlike_array(lhs) && ctype_is_strlike_array(rhs)) {
@@ -1732,7 +1738,8 @@ sema_pointer_cmp_impl(const struct ctype *lhs,
 			return make_result(ERR_SEMA_OPERAND_CHAR_ARRAY_SIZE);
 		} /* else: RHS string may be shorter than LHS capacity */
 	} else if (ctype_is_pointer(lhs) && ctype_is_pointer(rhs)) {
-		if (ctype_is_void_ptr(lhs) || ctype_is_void_ptr(rhs)) {
+		if (void_ptr_as_any_ptr &&
+		    (ctype_is_void_ptr(lhs) || ctype_is_void_ptr(rhs))) {
 			/* void* converts to/from any other pointer type */
 			// TODO: should we avoid arr->ptr decay here?
 		} else {
@@ -1743,13 +1750,16 @@ sema_pointer_cmp_impl(const struct ctype *lhs,
 	} else if (ctype_is_pointer(rhs) && (!ish || !ctype_nullptr_ish(lhs))) {
 		return make_result(ERR_SEMA_OPERAND_POINTER_RHS_VS_NOT_LHS);
 	}
+
 	return RESULT_OK;
 }
 
 static WARN_UNUSED result_t
 sema_pointer_cmp(const struct ctype *lhs, const struct ctype *rhs)
 {
-	check(sema_pointer_cmp_impl(lhs, rhs, true));
+	check(sema_pointer_cmp_impl(lhs,
+	                            rhs,
+	                            SPC_ZERO_AS_NULL | SPC_VOIDP_AS_ANY_PTR));
 	return RESULT_OK;
 }
 
@@ -1778,11 +1788,15 @@ sema_pointer(struct ast *a, void *userdata)
 		break;
 	case NODE_DECLARATION:
 		if (a->u.declare.init != NULL) {
+			uint32_t flags = SPC_VOIDP_AS_ANY_PTR;
+			/* do not allow zero->nullptr for array init */
+			if (!ctype_is_array(&a->u.declare.var_type)) {
+				flags |= SPC_ZERO_AS_NULL;
+			}
 			check(sema_pointer_cmp_impl(
 				&a->u.declare.var_type,
 				&a->u.declare.init->expr_type,
-				/* do not allow zero->nullptr for array init */
-				!ctype_is_array(&a->u.declare.var_type)));
+				flags));
 		}
 		break;
 	case NODE_SWITCH:
@@ -1844,7 +1858,7 @@ sema_pointer(struct ast *a, void *userdata)
 			check(sema_pointer_cmp_impl(
 				&a->u.op_binary.lhs->expr_type,
 				&a->u.op_binary.rhs->expr_type,
-				false));
+				0));
 		}
 		break;
 	case NODE_EXPRESSION_COMPARE_EQUAL:
@@ -1858,7 +1872,7 @@ sema_pointer(struct ast *a, void *userdata)
 	case NODE_EXPRESSION_COMPARE_MORE_THAN_EQ:
 		check(sema_pointer_cmp_impl(&a->u.op_binary.lhs->expr_type,
 		                            &a->u.op_binary.rhs->expr_type,
-		                            false));
+		                            0));
 		break;
 	case NODE_EXPRESSION_BINARY_MULTIPLY:
 	case NODE_EXPRESSION_BINARY_DIVIDE:
