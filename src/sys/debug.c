7b88c615e8aa68785961d3edf2fc9ce4a00fff44
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2026-01-08 18:34:12 -0800

    revert 546b22a, will try simpler approach

--- src/sys/debug.c
+++ src/sys/debug.c
@@ -1,16 +1,9 @@
 #include "sys/debug.h"
 
-#include <assert.h>
-#include <math.h> /* for log10() */
 #include <stdarg.h>
 #include <stdio.h>
-#include <string.h>    /* for strlen() */
-#include <sys/param.h> /* for MIN() */
 
-enum {
-	LOG_LVL_MAX_LEN = 5,
-	FILENAME_LINENO_COLUMN_WIDTH = 9,
-};
+static const int LOG_LVL_MAX_LEN = 5;
 
 static __attribute__((format(printf, 4, 0))) void
 vlog(const char *level,
@@ -19,30 +12,7 @@ vlog(const char *level,
      const char *pattern,
      va_list ap)
 {
-	const int lineno_digits = 1 + (int)log10(lineno);
-	assert(lineno_digits <= 4); /* source file contains <10k lines */
-
-	char fname_prefix[FILENAME_LINENO_COLUMN_WIDTH] = {0};
-	const size_t fname_capacity = sizeof(fname_prefix) - 1 - lineno_digits;
-
-	const char *end = memchr(fname, '.', strlen(fname));
-	assert(end != NULL); /* source filename ends with .c extension */
-	memcpy(fname_prefix, fname, MIN((size_t)(end - fname), fname_capacity));
-	const int width = (int)strlen(fname_prefix) + lineno_digits + 1;
-
-	int padding = 0;
-	if (width < FILENAME_LINENO_COLUMN_WIDTH) {
-		padding = FILENAME_LINENO_COLUMN_WIDTH - width;
-	}
-
-	fprintf(stderr,
-	        "%*s %s:%u%*s| ",
-	        LOG_LVL_MAX_LEN,
-	        level,
-	        fname_prefix,
-	        lineno,
-	        padding,
-	        "");
+	fprintf(stderr, "%-*s %s:%u: ", LOG_LVL_MAX_LEN, level, fname, lineno);
 	vfprintf(stderr, pattern, ap);
 	fputc('\n', stderr);
 }
