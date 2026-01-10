3a28cc5b6691f42c98ba8fdae91d6bb87d3ccec3
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2026-01-10 02:31:42 -0800

    cleanup: rm unnecessary ctype propagation in sema_subscript() rewrite; can let sema_expr_types() handle this; verified this does not regress past chapters

--- src/passes/sema.c
+++ src/passes/sema.c
@@ -934,7 +934,6 @@ sema_subscript(struct ast *a, void *userdata)
 	struct ast *new_node = arena_alloc(arena, sizeof(*new_node));
 	check_if(new_node == NULL, ERR_SEMA_ALLOC);
 	new_node->node_type = NODE_EXPRESSION_BINARY_ADD;
-	check(ctype_copy(arena, &a->expr_type, &new_node->expr_type));
 	new_node->u.op_binary = a->u.op_binary;
 
 	a->node_type = NODE_EXPRESSION_UNARY_DEREFERENCE;
