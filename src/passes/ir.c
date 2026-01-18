1fc5a840661977e7ec7c28953468802c4ecf4a0c
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2026-01-18 02:37:18 -0800

    fix ir_sizeof() on struct type, requires type_table parameter

--- src/passes/ir.c
+++ src/passes/ir.c
@@ -83,10 +83,12 @@ ir_val_jump_label(long long int jump_label, struct ir_val *dst)
 }
 
 static void
-ir_val_ctype_to_size(const struct ctype *c, struct ir_val *dst)
+ir_val_ctype_to_size(const struct ctype *c,
+                     struct intermediate *ir,
+                     struct ir_val *dst)
 {
 	dst->subtype = IR_VAL_CONSTANT;
-	dst->num = ctype_to_size_bytes(c);
+	dst->num = ctype_to_size_bytes_with_types(c, ir->env.types);
 	dst->c89type = LIKE_SIZE_T;
 }
 
@@ -962,7 +964,7 @@ ir_incr_decr(Arena *arena,
 		incr->args[1].subtype = IR_VAL_CONSTANT;
 		incr->args[1].num = pointer_index;
 		incr->args[1].c89type = LIKE_PTRDIFF_T;
-		ir_val_ctype_to_size(a->expr_type.referent, &incr->args[2]);
+		ir_val_ctype_to_size(a->expr_type.referent, ir, &incr->args[2]);
 		ir_val_copy(&incr->args[0], &incr->args[3]);
 	}
 
@@ -1087,7 +1089,9 @@ ir_unary_op(Arena *arena,
 }
 
 static WARN_UNUSED result_t
-ir_sizeof(const struct ast *a, struct ir_val *return_value)
+ir_sizeof(const struct ast *a,
+          struct intermediate *ir,
+          struct ir_val *return_value)
 {
 	assert(a->node_type == NODE_EXPRESSION_UNARY_SIZE_OF);
 	assert(ctype_is_equal(&a->expr_type, &LIKE_SIZE_T));
@@ -1095,7 +1099,7 @@ ir_sizeof(const struct ast *a, struct ir_val *return_value)
 	const struct ctype *inner_type = &a->u.op_unary.operand->expr_type;
 	assert(!ctype_is_void(inner_type));
 
-	ir_val_ctype_to_size(inner_type, return_value);
+	ir_val_ctype_to_size(inner_type, ir, return_value);
 	return RESULT_OK;
 }
 
@@ -1209,6 +1213,7 @@ ir_ptr_ptr_math(Arena *arena,
 	ir_val_copy(&binary_return, &divide->args[0]);
 
 	ir_val_ctype_to_size(a->u.op_binary.lhs->expr_type.referent,
+	                     ir,
 	                     &divide->args[1]);
 	assert(ctype_is_equal(&divide->args[1].c89type, &LIKE_SIZE_T));
 	check(ctype_copy(arena,
@@ -1288,7 +1293,9 @@ ir_ptr_math(Arena *arena,
 	}
 	assert(pos == 2);
 
-	ir_val_ctype_to_size(pointer->expr_type.referent, &ptr_plus->args[2]);
+	ir_val_ctype_to_size(pointer->expr_type.referent,
+	                     ir,
+	                     &ptr_plus->args[2]);
 
 	check(ir_val_tmpvar_gen(arena, ir, &a->expr_type, &ptr_plus->args[3]));
 	ctype_array_decay_to_pointer(&ptr_plus->args[3].c89type);
@@ -1786,7 +1793,7 @@ ir_expr(Arena *arena,
 		}
 		break;
 	case NODE_EXPRESSION_UNARY_SIZE_OF:
-		check(ir_sizeof(a, return_value));
+		check(ir_sizeof(a, ir, return_value));
 		break;
 	case NODE_EXPRESSION_CAST:
 		check(ir_cast(arena, a, ir, dst, return_value));
