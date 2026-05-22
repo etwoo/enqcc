etwoo/enqcc

commit 291bcfbe2cfe832b1494593557f596412c5d2ab2

ASan: fix partially uninitialized `struct ast`
==83174==ERROR: AddressSanitizer: SEGV on unknown address ...
==83174==The signal is caused by a READ memory access.
  #0 0x55610d0b2e40 in ctype_is_array src/lang/types.c:195
  #1 0x55610d0b2e40 in ctype_is_equal_impl src/lang/types.c:260
  #2 0x55610d0b2e40 in ctype_is_equal src/lang/types.c:282
  #3 0x55610d0b0050 in sema_pointer_cmp_impl src/passes/sema.c:1740
  #4 0x55610d0aea4d in sema_pointer_cmp src/passes/sema.c:1765
  #5 0x55610d0aea4d in sema_fn_signature_matches src/passes/sema.c:2227
  #6 0x55610d0aea4d in sema_fn_signature src/passes/sema.c:2346
  #7 0x55610d0ad412 in sema_walk src/passes/sema.c:245
  #8 0x55610d0ad458 in sema_walk src/passes/sema.c
  #9 0x55610d0aefc2 in sema_walk_flat src/passes/sema.c:366
  #10 0x55610d0ad58b in sema_walk src/passes/sema.c
  #11 0x55610d0ad458 in sema_walk src/passes/sema.c
  #12 0x55610d0aefc2 in sema_walk_flat src/passes/sema.c:366
  #13 0x55610d0ad58b in sema_walk src/passes/sema.c
  #14 0x55610d0ad277 in sema_typecheck src/passes/sema.c:2784
  #15 0x55610d099a60 in compile main.c:89
  #16 0x55610d099a60 in main main.c:208

src/passes/sema.c
@@ -897,6 +897,7 @@ sema_compound_assignment(struct ast *a, void *userdata)
 
 	struct ast *new_node = arena_alloc(arena, sizeof(*new_node));
 	check_if(new_node == NULL, ERR_SEMA_ALLOC);
+	memset(new_node, 0, sizeof(*new_node));
 	new_node->node_type = new_type;
 	check(ctype_copy(arena, &a->expr_type, &new_node->expr_type));
 	new_node->u.op_binary = a->u.op_binary;

commit f38b93cea40acd12c913d2bd5346cf05b6d43db9

add APIs for common arena_alloc() patterns
Consolidate various callers that arena_alloc() and then immediately
memset() to zero. Ditto for callers that arena_alloc() and then
deep-copy from a source struct.

This should reduce the risk of uninitialized memory leading to
unintended effects. See ASan fixes: 291bcfb, 3a112af, 05a0350.

main.c
@@ -17,7 +17,6 @@
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <string.h>
 #include <sys/param.h> /* for MAX() */
 #include <sysexits.h>
 #include <unistd.h> /* for close() */

src/lang/symbol.c
@@ -1,12 +1,12 @@
 #include "lang/symbol.h"
 
+#include "sys/alloc.h"
 #include "sys/array.h"
 #include "sys/debug.h"
 
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/param.h> /* for MAX() */
 
 bool
@@ -61,9 +61,8 @@ constant_set_zero(Arena *arena,
                   struct constant_initializer *ci)
 {
 	ci->count = get_initializer_element_count(c89type);
-	ci->elements = arena_alloc(arena, ci->count * sizeof(*ci->elements));
+	ci->elements = zalloc(arena, ci->count * sizeof(*ci->elements));
 	check_if(ci->elements == NULL, ERR_SEMA_ALLOC);
-	memset(ci->elements, 0, ci->count * sizeof(*ci->elements));
 
 	const long long unsigned element_size_bytes =
 		get_initializer_element_size_bytes(c89type);
@@ -80,7 +79,7 @@ constant_make_zero(Arena *arena,
                    const struct ctype *c89type,
                    struct constant_initializer **dst)
 {
-	*dst = arena_alloc(arena, sizeof(**dst));
+	*dst = zalloc(arena, sizeof(**dst));
 	check_if(*dst == NULL, ERR_SYMBOL_ALLOC);
 	check(constant_set_zero(arena, c89type, *dst));
 	return RESULT_OK;
@@ -145,9 +144,8 @@ symbols_prepend(Arena *arena,
                 enum symbol_type stype,
                 struct ctype *c89type)
 {
-	struct symbol *node = arena_alloc(arena, sizeof(*node));
+	struct symbol *node = zalloc(arena, sizeof(*node));
 	check_if(node == NULL, ERR_SYMBOL_ALLOC);
-	memset(node, 0, sizeof(*node));
 	node->name = *name;
 	node->stype = stype;
 	check(ctype_copy(arena, c89type, &node->c89type));
@@ -234,16 +232,9 @@ mangle_name(Arena *arena, struct symbol *s)
 	check_if(rc < 0, ERR_SYMBOL_ALLOC);
 
 	s->name.sz = strlen(mangled_str);
-
-	char *arena_copy = arena_alloc(arena, strlen(mangled_str));
-	if (arena_copy == NULL) {
-		free(mangled_str);
-		return make_result(ERR_SYMBOL_ALLOC);
-	}
-
-	memcpy(arena_copy, mangled_str, s->name.sz); /* exclude NUL */
-	s->name.data = arena_copy;
-
+	s->name.data = deepcopy(arena, mangled_str, s->name.sz);
 	free(mangled_str);
+
+	check_if(s->name.data == NULL, ERR_SYMBOL_ALLOC);
 	return RESULT_OK;
 }

src/lang/types.c
@@ -1,19 +1,19 @@
 #include "lang/types.h"
 
+#include "sys/alloc.h"
 #include "sys/array.h"
 
 #include <assert.h>
 #include <stdio.h>     /* for snprintf() */
-#include <string.h>    /* for memset */
+#include <string.h>    /* for strlcpy() */
 #include <sys/param.h> /* for MIN() and MAX() */
 
 result_t
 ctype_alloc(Arena *arena, struct ctype **dst)
 {
 	assert(dst != NULL && *dst == NULL);
-	*dst = arena_alloc(arena, sizeof(**dst));
+	*dst = zalloc(arena, sizeof(**dst));
 	check_if(*dst == NULL, ERR_CTYPE_ALLOC);
-	memset(*dst, 0, sizeof(**dst));
 	return RESULT_OK;
 }
 

src/passes/codegen.c
@@ -2,6 +2,7 @@
 
 #include "passes.h"
 #include "passes/ir.h"
+#include "sys/alloc.h"
 #include "sys/array.h"
 #include "sys/compiler_features.h"
 #include "sys/debug.h"
@@ -32,7 +33,7 @@ codegen_alloc_op(Arena *arena, struct asm_op **dst)
 {
 	static struct asm_op dummy_workaround_clang_analyzer_null_pointer = {0};
 
-	*dst = arena_alloc(arena, sizeof(**dst));
+	*dst = zalloc(arena, sizeof(**dst));
 	if (*dst == NULL) {
 		/*
 		 * See parse_init() for an explanation of this workaround.
@@ -41,7 +42,6 @@ codegen_alloc_op(Arena *arena, struct asm_op **dst)
 		return make_result(ERR_CODEGEN_ALLOC);
 	}
 
-	memset(*dst, 0, sizeof(**dst));
 	return RESULT_OK;
 }
 
@@ -1439,9 +1439,8 @@ codegen_function(Arena *arena,
                  struct asm_function **dst)
 {
 	assert(dst != NULL);
-	*dst = arena_alloc(arena, sizeof(**dst));
+	*dst = zalloc(arena, sizeof(**dst));
 	check_if(*dst == NULL, ERR_CODEGEN_ALLOC);
-	memset(*dst, 0, sizeof(**dst));
 
 	(**dst).identifier = ir->identifier;
 	(**dst).linkage = codegen_map_linkage(ir->linkage);
@@ -1465,9 +1464,8 @@ codegen_variable(Arena *arena,
                  struct asm_variable **dst)
 {
 	assert(dst != NULL);
-	*dst = arena_alloc(arena, sizeof(**dst));
+	*dst = zalloc(arena, sizeof(**dst));
 	check_if(*dst == NULL, ERR_CODEGEN_ALLOC);
-	memset(*dst, 0, sizeof(**dst));
 
 	(**dst).identifier = ir->identifier;
 	(**dst).linkage = codegen_map_linkage(ir->linkage);
@@ -1481,9 +1479,8 @@ codegen_string_literal(Arena *arena,
                        struct asm_str **dst)
 {
 	assert(dst != NULL);
-	*dst = arena_alloc(arena, sizeof(**dst));
+	*dst = zalloc(arena, sizeof(**dst));
 	check_if(*dst == NULL, ERR_CODEGEN_ALLOC);
-	memset(*dst, 0, sizeof(**dst));
 
 	(**dst).string_unique = ir->string_unique;
 	(**dst).initializer = ir->initializer;
@@ -1522,9 +1519,8 @@ codegen_program(Arena *arena,
 result_t
 codegen_init(Arena *arena, const struct intermediate *ir, struct assembly **cg)
 {
-	*cg = arena_alloc(arena, sizeof(**cg));
+	*cg = zalloc(arena, sizeof(**cg));
 	check_if(*cg == NULL, ERR_CODEGEN_ALLOC);
-	memset(*cg, 0, sizeof(**cg));
 	check(codegen_program(arena, ir, cg));
 	return RESULT_OK;
 }
@@ -1629,7 +1625,7 @@ codegen_replace_pseudo(Arena *arena, struct assembly *cg)
 		assert(size > 0);
 		assert(size <= 4096); /* if exceeded, refactor */
 
-		long long int *off = arena_alloc(arena, sizeof(*off) * size);
+		long long int *off = zalloc(arena, sizeof(*off) * size);
 		check(codegen_replace_pseudo_fn(f, range, off, false));
 
 		assert(f->stack_usage == 0);

src/passes/emit.c
@@ -1,5 +1,6 @@
 #include "passes.h"
 #include "passes/codegen.h"
+#include "sys/alloc.h"
 #include "sys/array.h"
 #include "sys/compiler_features.h"
 
@@ -8,7 +9,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>    /* for free() */
-#include <string.h>    /* for memcpy() */
+#include <string.h>    /* for strlen() */
 #include <sys/param.h> /* for MIN() */
 
 static const char LINUX_NX[] = "\t.section .note.GNU-stack,\"\",@progbits\n";
@@ -813,7 +814,7 @@ emit_asm_fp_check(Arena *arena,
 		}
 	}
 
-	struct fp_constant *node = arena_alloc(arena, sizeof(*node));
+	struct fp_constant *node = zalloc(arena, sizeof(*node));
 	check_if(node == NULL, ERR_EMIT_ALLOC);
 	node->next = *emitted;
 	node->value = value;

src/passes/ir.c
@@ -3,6 +3,7 @@
 #include "lang/symbol.h"
 #include "passes.h"
 #include "passes/parse.h"
+#include "sys/alloc.h"
 #include "sys/array.h"
 #include "sys/compiler_features.h"
 #include "sys/debug.h"
@@ -13,9 +14,8 @@
 static WARN_UNUSED result_t
 ir_alloc_op(Arena *arena, struct ir_op **dst)
 {
-	*dst = arena_alloc(arena, sizeof(**dst));
+	*dst = zalloc(arena, sizeof(**dst));
 	check_if(*dst == NULL, ERR_IR_ALLOC);
-	memset(*dst, 0, sizeof(**dst));
 	return RESULT_OK;
 }
 
@@ -795,9 +795,11 @@ ir_assignment(Arena *arena,
 			/*
 			 * Use opaque kludge.userdata pointer in AST node.
 			 */
-			struct ir_val *ud = arena_alloc(arena, sizeof(*ud));
+			struct ir_val *ud =
+				deepcopy(arena,
+			                 &compound_assign_glue_return,
+			                 sizeof(compound_assign_glue_return));
 			check_if(ud == NULL, ERR_IR_ALLOC);
-			memcpy(ud, &compound_assign_glue_return, sizeof(*ud));
 			assert(a->u.op_binary.lhs->kludge.userdata == NULL);
 			a->u.op_binary.lhs->kludge.userdata = ud;
 		}
@@ -1760,9 +1762,8 @@ ir_func(Arena *arena,
 	assert(a->node_type == NODE_FUNCTION);
 
 	assert(dst != NULL);
-	*dst = arena_alloc(arena, sizeof(**dst));
+	*dst = zalloc(arena, sizeof(**dst));
 	check_if(*dst == NULL, ERR_IR_ALLOC);
-	memset(*dst, 0, sizeof(**dst));
 
 	struct ir_function *f = *dst;
 	f->identifier = a->u.function.identifier.name;
@@ -1819,9 +1820,8 @@ static WARN_UNUSED result_t
 ir_var(Arena *arena, struct symbol *s, struct ir_variable **dst)
 {
 	assert(dst != NULL);
-	*dst = arena_alloc(arena, sizeof(**dst));
+	*dst = zalloc(arena, sizeof(**dst));
 	check_if(*dst == NULL, ERR_IR_ALLOC);
-	memset(*dst, 0, sizeof(**dst));
 
 	(**dst).identifier = s->name;
 	(**dst).linkage = ir_map_linkage(s->linkage.linkage);
@@ -1847,9 +1847,8 @@ static WARN_UNUSED result_t
 ir_string_literal(Arena *arena, struct symbol *s, struct ir_str **dst)
 {
 	assert(dst != NULL);
-	*dst = arena_alloc(arena, sizeof(**dst));
+	*dst = zalloc(arena, sizeof(**dst));
 	check_if(*dst == NULL, ERR_IR_ALLOC);
-	memset(*dst, 0, sizeof(**dst));
 
 	(**dst).string_unique = s->unique;
 	assert(s->linkage.initial == INITIAL_VALUE_CONSTANT);
@@ -1935,9 +1934,8 @@ ir_init(Arena *arena,
         struct symbol_table *sym,
         struct intermediate **ir)
 {
-	*ir = arena_alloc(arena, sizeof(**ir));
+	*ir = zalloc(arena, sizeof(**ir));
 	check_if(*ir == NULL, ERR_IR_ALLOC);
-	memset(*ir, 0, sizeof(**ir));
 	(**ir).env.generator = base_id;
 	(**ir).env.labels = base_label;
 	check(ir_program(arena, a, sym, *ir));

src/passes/lex.c
@@ -1,6 +1,7 @@
 #include "passes/lex.h"
 
 #include "passes.h"
+#include "sys/alloc.h"
 #include "sys/array.h"
 #include "sys/compiler_features.h"
 #include "sys/debug.h"
@@ -16,9 +17,8 @@
 static WARN_UNUSED result_t
 lex_alloc(Arena *arena, struct token **tok)
 {
-	*tok = arena_alloc(arena, sizeof(**tok));
+	*tok = zalloc(arena, sizeof(**tok));
 	check_if(*tok == NULL, ERR_LEX_ALLOC);
-	memset(*tok, 0, sizeof(**tok));
 	return RESULT_OK;
 }
 
@@ -330,7 +330,7 @@ map_span_to_strlike(Arena *arena,
                     const struct string_view *src,
                     struct string_view *dst)
 {
-	char *out = arena_alloc(arena, src->sz); /* source size -> capacity */
+	char *out = zalloc(arena, src->sz); /* source size -> capacity */
 	dst->data = out;
 	dst->sz = 0;
 

src/passes/parse/alloc.c
@@ -1,17 +1,16 @@
 #include "passes/parse/alloc.h"
 
 #include "passes/parse.h"
+#include "sys/alloc.h"
 
 #include <assert.h>
-#include <string.h> /* for memset() */
 
 result_t
 parse_alloc(Arena *arena, struct ast **dst, unsigned nt)
 {
 	assert(dst != NULL && *dst == NULL);
-	*dst = arena_alloc(arena, sizeof(**dst));
+	*dst = zalloc(arena, sizeof(**dst));
 	check_if(*dst == NULL, ERR_PARSE_ALLOC);
-	memset(*dst, 0, sizeof(**dst));
 	(**dst).node_type = nt;
 	return RESULT_OK;
 }
@@ -29,8 +28,7 @@ result_t
 flat_alloc(Arena *arena, struct flat **dst)
 {
 	assert(dst != NULL && *dst == NULL);
-	*dst = arena_alloc(arena, sizeof(**dst));
+	*dst = zalloc(arena, sizeof(**dst));
 	check_if(*dst == NULL, ERR_PARSE_ALLOC);
-	memset(*dst, 0, sizeof(**dst));
 	return RESULT_OK;
 }

src/passes/parse/declaration.c
@@ -8,12 +8,12 @@
 #include "passes/parse/constant.h"
 #include "passes/parse/expression.h"
 #include "passes/parse/token.h"
+#include "sys/alloc.h"
 #include "sys/array.h"
 #include "sys/debug.h"
 
 #include <assert.h>
 #include <limits.h> /* for ULLONG_MAX */
-#include <string.h> /* for memset() */
 
 struct parse_basic_type_state {
 	size_t n_void;
@@ -227,9 +227,8 @@ parse_function_params(Arena *arena,
 	}
 	if (count > 0) {
 		size_t bytes = sizeof(**dst) * (count + 1);
-		*dst = arena_alloc(arena, bytes);
+		*dst = zalloc(arena, bytes);
 		check_if(*dst == NULL, ERR_PARSE_ALLOC);
-		memset(*dst, 0, bytes);
 		check(parse_function_params_impl(arena, tok, dst, &count));
 	}
 
@@ -428,9 +427,8 @@ parse_declarator_group_split_impl(Arena *arena,
 			dst_token = &(**dst_token).next;
 		}
 
-		*dst_token = arena_alloc(arena, sizeof(**dst_token));
+		*dst_token = deepcopy(arena, *tok, sizeof(**dst_token));
 		check_if(*dst_token == NULL, ERR_PARSE_ALLOC);
-		memcpy(*dst_token, *tok, sizeof(**dst_token));
 		(**dst_token).next = NULL;
 
 		if (is_token_type(*tok, TOKEN_IDENTIFIER)) {
@@ -474,9 +472,8 @@ parse_declarator_group_split(Arena *arena,
 {
 	assert(dst != NULL && *dst == NULL);
 
-	*dst = arena_alloc(arena, sizeof(**dst));
+	*dst = zalloc(arena, sizeof(**dst));
 	check_if(*dst == NULL, ERR_PARSE_ALLOC);
-	memset(*dst, 0, sizeof(**dst));
 
 	bool done = false;
 	bool got_identifier = false;

src/passes/sema.c
@@ -2,6 +2,7 @@
 #include "passes.h"
 #include "passes/parse.h"
 #include "passes/parse/alloc.h"
+#include "sys/alloc.h"
 #include "sys/compiler_features.h"
 #include "sys/debug.h"
 
@@ -218,8 +219,7 @@ map_numeric_type(Arena *arena,
 {
 	out->count = count_initializer_elements(dst_type, init);
 	assert(out->count > 0);
-	out->elements = arena_alloc(arena, out->count * sizeof(*out->elements));
-	memset(out->elements, 0, out->count * sizeof(*out->elements));
+	out->elements = zalloc(arena, out->count * sizeof(*out->elements));
 	check_if(out->elements == NULL, ERR_SEMA_ALLOC);
 	struct constant_bytes *cursor = out->elements;
 	populate_initializer_elements(init, dst_type, &cursor);
@@ -546,16 +546,14 @@ make_case(Arena *arena,
           struct ctype *control_type,
           int128_t new_value)
 {
-	*dst = arena_alloc(arena, sizeof(**dst));
+	*dst = zalloc(arena, sizeof(**dst));
 	check_if(*dst == NULL, ERR_SEMA_ALLOC);
-	memset(*dst, 0, sizeof(**dst));
 
 	(**dst).node_type = NODE_CASE;
 	(**dst).u.case_.unique = existing_unique;
 
-	struct ast *new_node = arena_alloc(arena, sizeof(*new_node));
+	struct ast *new_node = zalloc(arena, sizeof(*new_node));
 	check_if(new_node == NULL, ERR_SEMA_ALLOC);
-	memset(new_node, 0, sizeof(*new_node));
 
 	new_node->node_type = NODE_CONSTANT;
 	new_node->u.num = new_value;
@@ -588,9 +586,8 @@ case_prepend(Arena *arena,
 		}
 	}
 
-	struct flat *node = arena_alloc(arena, sizeof(*node));
+	struct flat *node = zalloc(arena, sizeof(*node));
 	check_if(node == NULL, ERR_SEMA_ALLOC);
-	memset(node, 0, sizeof(*node));
 
 	/*
 	 * Synthesize NODE_CASE equivalent to <new_case>, only with
@@ -772,9 +769,8 @@ labels_prepend(struct sema_label_gotos_state *state,
 		return RESULT_OK;
 	}
 
-	struct label *node = arena_alloc(state->arena, sizeof(*node));
+	struct label *node = zalloc(state->arena, sizeof(*node));
 	check_if(node == NULL, ERR_SEMA_ALLOC);
-	memset(node, 0, sizeof(*node));
 
 	node->name = *name;
 	node->id = ++state->generator;
@@ -896,9 +892,8 @@ sema_compound_assignment(struct ast *a, void *userdata)
 		return RESULT_OK;
 	}
 
-	struct ast *new_node = arena_alloc(arena, sizeof(*new_node));
+	struct ast *new_node = zalloc(arena, sizeof(*new_node));
 	check_if(new_node == NULL, ERR_SEMA_ALLOC);
-	memset(new_node, 0, sizeof(*new_node));
 	new_node->node_type = new_type;
 	check(ctype_copy(arena, &a->expr_type, &new_node->expr_type));
 	new_node->u.op_binary = a->u.op_binary;
@@ -928,9 +923,8 @@ sema_subscript(struct ast *a, void *userdata)
 		return RESULT_OK;
 	}
 
-	struct ast *new_node = arena_alloc(arena, sizeof(*new_node));
+	struct ast *new_node = zalloc(arena, sizeof(*new_node));
 	check_if(new_node == NULL, ERR_SEMA_ALLOC);
-	memset(new_node, 0, sizeof(*new_node));
 	new_node->node_type = NODE_EXPRESSION_BINARY_ADD;
 	check(ctype_copy(arena, &a->expr_type, &new_node->expr_type));
 	new_node->u.op_binary = a->u.op_binary;
@@ -2075,9 +2069,8 @@ sema_alloc_auxiliary(Arena *arena, void **out_as_void_pp)
 	struct sema_symbol_auxiliary **out =
 		(struct sema_symbol_auxiliary **)out_as_void_pp;
 	assert(*out == NULL);
-	*out = arena_alloc(arena, sizeof(**out));
+	*out = zalloc(arena, sizeof(**out));
 	check_if(*out == NULL, ERR_SYMBOL_ALLOC);
-	memset(*out, 0, sizeof(**out));
 	return RESULT_OK;
 }
 
@@ -2144,8 +2137,7 @@ sema_fn_decl_collect(Arena *arena,
 	}
 
 	*n_args = count;
-	*param_types = arena_alloc(arena, count * sizeof(**param_types));
-	memset(*param_types, 0, count * sizeof(**param_types));
+	*param_types = zalloc(arena, count * sizeof(**param_types));
 
 	count = 0;
 	FOREACH_FUNCTION_PARAMETER (cur, a->u.function.params) {
@@ -2182,8 +2174,7 @@ sema_fn_call_collect(Arena *arena,
 	}
 
 	*n_args = count;
-	*param_types = arena_alloc(arena, count * sizeof(**param_types));
-	memset(*param_types, 0, count * sizeof(**param_types));
+	*param_types = zalloc(arena, count * sizeof(**param_types));
 
 	count = 0;
 	for (struct flat *z = a->u.call.args; z != NULL; z = z->cdr) {

src/sys/CMakeLists.txt
@@ -1,3 +1,4 @@
 target_sources(compiler PRIVATE
+	alloc.c
 	debug.c
 )

src/sys/alloc.c
@@ -1,0 +1,25 @@
+#include "sys/alloc.h"
+
+#include "arena.h"
+
+#include <string.h> /* for memset(), memcpy() */
+
+void *
+zalloc(Arena *arena, size_t sz)
+{
+	void *dst = arena_alloc(arena, sz);
+	if (dst != NULL) {
+		memset(dst, 0, sz);
+	}
+	return dst;
+}
+
+void *
+deepcopy(Arena *arena, const void *src, size_t sz)
+{
+	void *dst = arena_alloc(arena, sz);
+	if (dst != NULL) {
+		memcpy(dst, src, sz);
+	}
+	return dst;
+}

src/sys/alloc.h
@@ -1,0 +1,9 @@
+#ifndef ALLOC_H
+#define ALLOC_H
+
+#include "arena.h"
+
+void *zalloc(Arena *arena, size_t sz);
+void *deepcopy(Arena *arena, const void *src, size_t sz);
+
+#endif